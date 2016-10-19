704f9562ecf6cf37ed10935239d99dc205f9acdb
Cryru <TheCryru@gmail.com>
2016-10-19 17:04:38 +0300

    Patch for 0.74
    Forgot to change the version variable.

--- src/Engine/Core/Core.cs
+++ src/Engine/Core/Core.cs
@@ -24,7 +24,7 @@ namespace SoulEngine
         #region "Declarations"
         //Engine Information
         public static string Name = "Soul Engine"; //The name of the engine.
-        public static string Ver = "0.72"; //The version of the engine.
+        public static string Ver = "0.74"; //The version of the engine.
         public static string GUID = "130F150C-0000-0000-0000-050E07090E05"; //The guid of the application. (Default Soul Engine - 130F150C-0000-0000-0000-050E07090E05)
 
         //Debug Variables and Objects
