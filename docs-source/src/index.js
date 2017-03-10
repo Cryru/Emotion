ded559788d2c0bab5cd5688e37a8bcfe0a90cb08
Vladimir Abadzhiev <vladimir.abadzhiev@softwaregroup-bg.com>
2017-03-10 16:47:10 +0200

    Documentation Update
    Legacy documentation page mostly complete.

--- docs-source/src/index.js
+++ docs-source/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
 import { Router, Route, Link, browserHistory } from 'react-router'
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <Router history={browserHistory}>
