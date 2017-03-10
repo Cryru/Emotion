a7ec4f8dae454639561ba64397597b8cd76f0eb5
Vlad Abadzhiev <TheCryru@gmail.com>
2017-03-10 16:47:10 +0200

    Documentation Update
    Legacy documentation page mostly complete.

--- docs-source/src/Pages/Legacy.js
+++ docs-source/src/Pages/Legacy.js
@@ -5,36 +5,101 @@ import style from '../pages.css';
 import Highlight from 'react-highlight';
 
 export default class Legacy extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-      return  (
-        <div>
-            <Row className='row-reset'>
-                <Col xs='12' className='col-reset pagePaddingHeader'>
-                    <div>tetete</div>
-                </Col>
-            </Row>
-            <Row className='row-reset'>
-                <Col xs='6' className='col-reset pagePadding'>
-                    <p className='pageText'>adadasdasdasdsadadsadasdasdasdasda</p>
-                    <Highlight className='cs'>
-{`string input = Console.ReadLine();
-Console.Clear();
-
-//comment highlight
-string[] split = input.Split(' ');
-
-for (int i = 0; i < split.Length; i++)
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <Row className='row-reset'>
+                    <Col xs={{ size: 'xs-12' }} className='col-reset pagePaddingHeader'>
+                        <div className='pageHeader-h1'>Legacy Support for SoulEngine 2016</div>
+                    </Col>
+                </Row>
+                <div className='pagePadding'>
+                    <Row className='row-reset'>
+                        <Col xs="10" className='col-reset'>
+                            <p className='pageText'>
+                                {`To allow old projects to be ported over a compatibility layer for older SoulEngine versions
+                        is located in the`} <a className='filePath' href="https://github.com/Cryru/SoulEngine/tree/master/src/Legacy">src/Engine/Legacy</a> {`folder, and
+                        can be accessed through the SoulEngine.Legacy namespace.`}
+                            </p>
+                            <p className='pageText'>
+                                {`The isn't direct copy-pasted from the older version, rather it is a syntax converter which makes use of new features behind the scenes.
+                        The reason for this is to prevent duplicate code and polution such as having two separate settings for window width for example.
+                        As such it is not guaranteed that both will behave the same in all situations.`}
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row className='row-reset'>
+                        <Col xs="5" className='col-reset'>
+                            <Highlight className='cs'>
+                                {
+                                    `public ObjectBase(Texture Image = null)
 {
-    split += true ? "test" : "falsetest";
-}
-`}
-                    </Highlight>
-                </Col>
-            </Row>
-        </div>
-    );
-   }
+    if (Image == null)
+    {
+        Image = new Texture();
+    }
+
+    this.Image = Image;
+}`
+                                }
+                            </Highlight>
+                            <span>Original Code</span>
+                        </Col>
+                        <Col xs="5" className='col-reset'>
+                            <Highlight className='cs'>
+                                {
+                                    `public ObjectBase(Texture Image = null)
+{
+    actualObject = GameObject.GenericDrawObject;
+
+    if (Image == null)
+    {
+        actualObject.Component<ActiveTexture>().Texture = Image.Image;
+    }
+}`
+                                }
+                            </Highlight>
+                            <span>Adapted Code</span>
+                        </Col>
+                    </Row>
+                    <div className='spacer' />
+                    <Row className='row-reset'>
+                        <Col xs="10" className='col-reset'>
+                            <p className='pageText'>
+                                {`The current version (2017 1 Development - 3/10/2017) fully implements the legacy Core and Settings.`}
+                            </p>
+                            <div className='spacer' />
+                            <div className='pageHeader-h2'>How To Port Over</div>
+                             <p className='pageText'>
+                                {`TODO`}
+                            </p>
+                        </Col>
+                    </Row>
+                    <div className='spacer' />
+                    <Row className='row-reset'>
+                        <Col xs="6" className='col-reset'>
+                            <p className='pageText'>
+                                {`Namespaces: `}
+                            </p>
+                            <ul>
+                                <li>SoulEngine.Legacy</li>
+                                <li>SoulEngine.Legacy.Objects</li>
+                            </ul>
+                        </Col>
+                         <Col xs="6" className='col-reset'>
+                            <p className='pageText'>
+                                {`Useful Links: `}
+                            </p>
+                            <ul>
+                                <li><a href="https://github.com/Cryru/SoulEngine-2016">SoulEngine 2016</a></li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        );
+    }
 }
