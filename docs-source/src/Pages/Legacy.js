a28fbbf2f4a0ca2726b2357c0cdae2af155a6896
Vlad Abadzhiev <TheCryru@gmail.com>
2017-03-10 15:25:18 +0200

    Documentation Skeleton

--- docs-source/src/Pages/Legacy.js
+++ docs-source/src/Pages/Legacy.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Row, Col } from 'reactstrap';
+import style from '../pages.css';
+import Highlight from 'react-highlight';
 
-export default page =
-(
-    <div className='col-xs-6'>
+export default class Legacy extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+      return  (
+        <div>
+            <Row className='row-reset'>
+                <Col xs='12' className='col-reset pagePaddingHeader'>
+                    <div>tetete</div>
+                </Col>
+            </Row>
+            <Row className='row-reset'>
+                <Col xs='6' className='col-reset pagePadding'>
+                    <p className='pageText'>adadasdasdasdsadadsadasdasdasdasda</p>
+                    <Highlight className='cs'>
+{`string input = Console.ReadLine();
+Console.Clear();
 
-    </div>
-);
+//comment highlight
+string[] split = input.Split(' ');
+
+for (int i = 0; i < split.Length; i++)
+{
+    split += true ? "test" : "falsetest";
+}
+`}
+                    </Highlight>
+                </Col>
+            </Row>
+        </div>
+    );
+   }
+}
