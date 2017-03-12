11759073b622a9f051ebacd6d222c4a264c45456
Cryru <TheCryru@gmail.com>
2017-03-12 15:41:50 +0200

    Documentation Update
    Column styling fixed.
    Routes fixed.

--- docs-source/src/Main.js
+++ docs-source/src/Main.js
@@ -31,20 +31,20 @@ export default class Main extends Component {
     }
 
     const navigationBar = [
-      <div className={navBoxClass}><Link to="/"><div className='navHeaderLink'><img className='logoImage' src={logo} /> SoulEngine Documentation</div></Link></div>,
-      <SidebarItem href='/page/object'><div className='navItem'>Object-Component System</div></SidebarItem>,
-      <SidebarItem href='/page/textures'><div className='navItem'>Animated Textures</div></SidebarItem>,
-      <SidebarItem href='/page/assets'><div className='navItem'>Assets</div></SidebarItem>,
-      <SidebarItem href='/page/scene'><div className='navItem'>Scene System</div></SidebarItem>,
-      <SidebarItem href='/page/event'><div className='navItem'>Event System</div></SidebarItem>,
-      <SidebarItem href='/page/tiled'><div className='navItem'>Tiled</div></SidebarItem>,
-      <SidebarItem href='/page/timing'><div className='navItem'>FPS and Timing</div></SidebarItem>,
-      <SidebarItem href='/page/tamper'><div className='navItem'>Asset Tampering Protection</div></SidebarItem>,
-      <SidebarItem href='/page/camera'><div className='navItem'>Camera System</div></SidebarItem>,
-      <SidebarItem href='/page/resolution'><div className='navItem'>Resolution Adaptation</div></SidebarItem>,
-      <SidebarItem href='/page/jsonfiles'><div className='navItem'>JSON File Management</div></SidebarItem>,
-      <SidebarItem href='/page/errors'><div className='navItem'>Error Logging</div></SidebarItem>,
-      <SidebarItem href='/page/legacy'><div className='navItem'>Legacy Support</div></SidebarItem>
+      <div className={navBoxClass}><Link to="/SoulEngine"><div className='navHeaderLink'><img className='logoImage' src={logo} /> SoulEngine Documentation</div></Link></div>,
+      <SidebarItem href='/SoulEngine/page/object'><div className='navItem'>Object-Component System</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/textures'><div className='navItem'>Animated Textures</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/assets'><div className='navItem'>Assets</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/scene'><div className='navItem'>Scene System</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/event'><div className='navItem'>Event System</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/tiled'><div className='navItem'>Tiled</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/timing'><div className='navItem'>FPS and Timing</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/tamper'><div className='navItem'>Asset Tampering Protection</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/camera'><div className='navItem'>Camera System</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/resolution'><div className='navItem'>Resolution Adaptation</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/jsonfiles'><div className='navItem'>JSON File Management</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/errors'><div className='navItem'>Error Logging</div></SidebarItem>,
+      <SidebarItem href='/SoulEngine/page/legacy'><div className='navItem'>Legacy Support</div></SidebarItem>
     ]
 
     return (
