7f75bb3cb5457f9f35692f3ca60c4deea9ba2f22
Cryru <TheCryru@gmail.com>
2017-05-09 22:29:38 +0300

    Version 6 Development
    Added a UDP socket for remote debugging.
    - Added a setting for the socket port.
    Improved controlling the blinker on the console.

--- src/Engine/Debugging/DebugSocket.cs
+++ src/Engine/Debugging/DebugSocket.cs
@@ -1,12 +1,85 @@
-﻿using System;
+﻿using Jint;
+using Jint.Runtime.Interop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using SoulEngine.Scripting;
 
 namespace SoulEngine.Debugging
 {
     public static class DebugSocket
     {
+        /// <summary>
+        /// The socket to be used for broadcasting.
+        /// </summary>
+        public static UdpClient Socket;
+
+        /// <summary>
+        /// The list of attached debuggers.
+        /// </summary>
+        private static List<IPEndPoint> AttachedDebuggers;
+
+        /// <summary>
+        /// Initiate broadcasting debug information.
+        /// </summary>
+        public static void Setup()
+        {
+            //Check if debugging is enabled.
+            if (Settings.Debug == false) return;
+
+            //Initiate the socket.
+            Socket = new UdpClient(new IPEndPoint(IPAddress.Any, Settings.DebugSocketPort));
+
+            //Create a list for attached debuggers.
+            AttachedDebuggers = new List<IPEndPoint>();
+
+            //Open script access.
+            ScriptEngine.Interpreter = new Engine(cfg => cfg.AllowClr());
+            ScriptEngine.Interpreter.Execute("var SoulEngine = importNamespace(\"SoulEngine\")");
+
+            //Listen for messages.
+            Socket.BeginReceive(ReceivedMessage, null);
+        }
+
+        /// <summary>
+        /// When a message is received.
+        /// </summary>
+        /// <param name="ar"></param>
+        private static void ReceivedMessage(IAsyncResult AR)
+        {
+            IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            AttachedDebuggers.Add(receivedIpEndPoint);
+            var receivedBytes = Socket.EndReceive(AR, ref receivedIpEndPoint);
+            string message = Encoding.UTF8.GetString(receivedBytes);
+
+            //Execute through the script engine.
+            Broadcast(Scripting.ScriptEngine.ExecuteScript(message).ToString());
+
+            //Resume listening.
+            Socket.BeginReceive(ReceivedMessage, null);
+        }
+
+        /// <summary>
+        /// Send a message to all connected debuggers.
+        /// </summary>
+        /// <param name="Message"></param>
+        public static void Broadcast(string Message)
+        {
+            for (int i = 0; i < AttachedDebuggers.Count; i++)
+            {
+                var send = Encoding.UTF8.GetBytes(Message);
+                //Start sending, call the sent method.
+                Socket.BeginSend(send, send.Length, AttachedDebuggers[i], new AsyncCallback(SendEndCallback), null);
+            }
+        }
+
+        private static void SendEndCallback(IAsyncResult AR)
+        {
+            Socket.EndSend(AR);
+        }
     }
 }
