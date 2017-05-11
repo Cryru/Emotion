6c7da4ec7de5669841d55ec0686748c23603dd13
Cryru <TheCryru@gmail.com>
2017-05-11 22:24:48 +0300

    Version 6 Development
    Added setting for pausing when losing focus.
    DebugSocket draw event established.

--- src/Engine/Core/Core.cs
+++ src/Engine/Core/Core.cs
@@ -76,7 +76,7 @@ namespace SoulEngine
         /// </summary>
         public event EventHandler<EventArgs> OnSizeChanged;
         /// <summary>
-        /// Triggered when the game's display mode changes.
+        /// Triggered when the game\"s display mode changes.
         /// </summary>
         public event EventHandler<EventArgs> OnDisplayModeChanged;
         #endregion
@@ -150,8 +150,8 @@ namespace SoulEngine
         /// </summary>
         protected override void Update(GameTime gameTime)
         {
-            //If the game is not focused, don't update.
-            if (IsActive == false) return;
+            //If the game is not focused, don\"t update.
+            if (IsActive == false && Settings.PauseOnFocusLoss) return;
 
             //Check if a scene is waiting to be loaded and if so load it.
             if (sceneLoadQueue != null) SceneLoad();
@@ -179,16 +179,17 @@ namespace SoulEngine
         /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            //If the game is not focused, don't update.
-            //if (IsActive == false) return;
-
             //Record frametime.
             frameTime = (float) gameTime.ElapsedGameTime.TotalMilliseconds;
 
-            DebugSocket.Broadcast("Drew frame in " + frameTime);
+            //Draw debug event, reports the current frametime. Used to determine FPS and activity.
+            DebugSocket.Broadcast("{\"type\": \"draw\", \"data\": \"" + frameTime + "\"}");
 
             //Start drawing frame by first clearing the screen, first the behind and then the front.
             Context.Graphics.Clear(Color.Black);
+
+            //If the game is not focused, don\"t update.
+            if (IsActive == false && Settings.PauseOnFocusLoss) return;
 
             //Allow composing.
             __composeAllowed = true;
@@ -199,7 +200,7 @@ namespace SoulEngine
             //Compose textures on the current scene. We draw the render targets before anything else because it renders over other things otherwise.
             Scene.Compose();
 
-            //Stop allowing composig. This is to prevent the 'black screen bug'.
+            //Stop allowing composig. This is to prevent the \"black screen bug\".
             __composeAllowed = false;
 
             Context.ink.Start(DrawChannel.Screen);
@@ -255,7 +256,7 @@ namespace SoulEngine
         #region "Event Rerouting"
         /// <summary>
         /// Is triggered when text input is detected to the game.
-        /// This returns the character as input, for instance a backspace is '/b' and so on.
+        /// This returns the character as input, for instance a backspace is \"/b\" and so on.
         /// </summary>
         private void Input_TextInput(object sender, TextInputEventArgs e)
         {
