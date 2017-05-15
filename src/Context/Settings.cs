04f6333978e305268da4d3dc7548649207bd5352
Cryru <TheCryru@gmail.com>
2017-05-16 00:30:27 +0300

    Cleanup for SE 2018

---
+++ src/Context/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SoulEngine
+{
+    //////////////////////////////////////////////////////////////////////////////
+    // SoulEngine - A game engine based on the MonoGame Framework.              //
+    // Public Repository: https://github.com/Cryru/SoulEngine                   //
+    //////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// The engine's user settings.
+    /// </summary>
+    public class Settings
+    {
+        public string WindowName = "SoulEngine";
+        public int WindowWidth = 960;
+        public int WindowHeight = 540;
+    }
+}
