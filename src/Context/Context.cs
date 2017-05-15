521f6ab71880f5b233b1cba77c61c8c0e4378ad5
Cryru <TheCryru@gmail.com>
2017-05-16 00:30:27 +0300

    Cleanup for SE 2018

--- src/Context/Context.cs
+++ src/Context/Context.cs
@@ -1,29 +1,20 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SoulEngine.Objects.Components.Helpers
+namespace SoulEngine
 {
     //////////////////////////////////////////////////////////////////////////////
     // SoulEngine - A game engine based on the MonoGame Framework.              //
     // Public Repository: https://github.com/Cryru/SoulEngine                   //
     //////////////////////////////////////////////////////////////////////////////
     /// <summary>
-    /// Render data about a single character.
+    /// The context objects holder.
     /// </summary>
-    public class DrawData
+    public class Context
     {
-        public float X;
-        public float Y;
-
-        public DrawData(float X, float Y)
-        {
-            this.X = X;
-            this.Y = Y;
-        }
+        public Settings Settings;
     }
 }
---
+++ src/Engine/Core.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SoulEngine
+{
+    //////////////////////////////////////////////////////////////////////////////
+    // SoulEngine - A game engine based on the MonoGame Framework.              //
+    // Public Repository: https://github.com/Cryru/SoulEngine                   //
+    //////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// The game instance for use by the engine.
+    /// </summary>
+    public class Core
+    {
+      
+    }
+}
+
