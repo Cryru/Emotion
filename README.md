5fe72e48785f2ab4b3da802d7f0af9fe52d076f6
Cryru <TheCryru@gmail.com>
2017-08-01 19:22:21 +0300

    SoulEngine 2018 Development Started

--- README.md
+++ README.md
@@ -1,62 +1,9 @@
-<img src="soulengine.png" width=30%>
+SoulEngine 2018 Development
 
-### [Documentation and Help](https://cryru.github.io/SE-Docs/)
+Plan:
 
-
-
-## What is it?
-
-SoulEngine is a 2D Monogame based game engine based on the idea of low level code writing, flexibiliy, and DIY, as opposed to the script based, high level, tool approach big engines have. Despite this, the point is not to complicate development, but rather make it easier and simpler while maintaining control. The end goal is to shorten the time it takes between coming up with an idea, getting a prototype ready, and eventually releasing a product.
-
-## Updates
-
-While I try to update the engine as often as possible, that is not always possible as life gets in the way, so most of the big changes and rewrites are left to big yearly updates. This year's version can be found here and is in development, while older versions can be found at these links:
-
-* [SoulEngine 2016](https://github.com/Cryru/SoulEngine-2016)
-* [SoulEngine 2016 Android](https://github.com/Cryru/SoulEngine-2016-Android)
-
-## This Year's Idea
-
-SoulEngine 2016 wasn't based on any particular idea and was pretty much over all the place. The 2017 version's philosophy is based on an entity-component system, as well as a single loaded "Scene", as opposed to the old Photoshop-like layer model.
-
-## Features and Progress
-
-- Object-Component System [&#10003;]
-- Simple Animated Textures [&#10003;]
-- Easy and Efficient Asset Loading [&#10003;]
-- Scene System [&#10003;]
-- Event System [&#10003;]
-- Tiled Integration [&#10003;]
-- FPS Independent Timing [&#10003;]
-- Asset Tampering Protection [&#10003;]
-- Camera System [&#10003;]
-- Resolution Adaptation with Boxing [&#10003;]
-- JSON Based File Management (For Settings, Save Files etc.) [&#10003;]
-- Error Logging [&#10003;]
-- Debugging Console [&#10003;]
-- Sound Engine [&#10003;]
-- Custom Text Rendering with Markup [&#10003;]
-- Scripting Support [&#10003;]
-- Documentation
-
-and more...
-
-## Projects For SoulEngine
-
-["SoulLib"](https://github.com/Cryru/SoulLib) - Encryption, JSON, Managed Files, and IO.
-
-["Polygon-Tool"](https://github.com/Cryru/SE-Polygon-Tool) - Polygon collision boxes for physics engine.
-
-["Asset-Meta-Generator"](https://github.com/Cryru/SE-Asset-Meta-Generator) - Generating Meta files for asset validation.
-
-["FileEncrypt"](https://github.com/Cryru/FileEncrypt) - Used to encrypt files.
-
-["Farseer Physics for SoulEngine"](https://github.com/Cryru/SE-Farseer) - Physics engine.
-
-["SoulEngine Docs"](https://github.com/Cryru/SE-Docs) - Online documentation.
-
-## External Projects
-
-["TiledSharp"](https://github.com/marshallward/TiledSharp) - Parsing .tmx files.
-
-["Jint"](https://github.com/sebastienros/jint) - Javascript interpretation for the script engine.
+- Use Raya
+	- Windows
+	- Linux
+- Use SoulEngine 2017 for Android
+- Compile the engine as a series of DLLs to be referenced by a project.
---
+++ SoulEngine/Engine.cs
@@ -0,0 +1,135 @@
+﻿using Raya.Enums;
+using Raya.Graphics;
+using Raya.Graphics.Primitives;
+using Raya.System;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SoulEngine
+{
+    public class Engine
+    {
+        public Context RayaContext;
+
+        public Engine()
+        {
+            Console.WriteLine("Starting using Raya version " + RayaSettings.Version);
+
+            // Test boot time.
+            Stopwatch runTest = new Stopwatch();
+            runTest.Start();
+
+            RayaContext = new Context();
+            Window RayaWindow = RayaContext.CreateWindow();
+
+            // Boot ready.
+            runTest.Stop();
+            Console.WriteLine("Raya loaded in: " + runTest.ElapsedMilliseconds + " ms");
+
+            // Run console thread.
+            Thread consoleThread = new Thread(new ThreadStart(ConsoleThread));
+            consoleThread.Start();
+
+            // Main loop.
+            int detail = 5;
+
+            Image Image = new Image((uint)(960 / detail), (uint)(540 / detail));
+            Texture Texture = new Texture(Image);
+            Sprite Sprite = new Sprite(Texture);
+            Sprite.Size = new Vector2(960, 540);
+
+            Font font = new Font("data/arial.ttf");
+            Text fpsMeter = new Text(RayaContext.FPS.ToString(), font);
+            fpsMeter.FillColor = Color.Yellow;
+            fpsMeter.OutlineColor = Color.Black;
+            fpsMeter.OutlineThickness = 2;
+            fpsMeter.CharacterSize = 17;
+            fpsMeter.Position = new Vector2f(5, 5);
+
+            while (RayaContext.Running)
+            {
+                //test.Rotate(5f);
+                switch (command)
+                {
+                    case "borderless":
+                        RayaContext.Window.Mode = WindowMode.Borderless;
+                        command = "";
+                        break;
+                    case "fullscreen":
+                        RayaContext.Window.Mode = WindowMode.Fullscreen;
+                        command = "";
+                        break;
+                    case "normal":
+                        RayaContext.Window.Mode = WindowMode.Window;
+                        command = "";
+                        break;
+                    case "resize":
+                        RayaContext.Window.Resizable = true;
+                        command = "";
+                        break;
+                    case "nosize":
+                        RayaContext.Window.Resizable = false;
+                        command = "";
+                        break;
+                }
+
+                if (command.Length > 6 && command.Substring(0, 6) == "detail")
+                {
+                    string[] split = command.Split(' ');
+                    detail = int.Parse(split[1]);
+
+                    Image = new Image((uint)(960 / detail), (uint)(540 / detail));
+                    Texture = new Texture(Image);
+                    Sprite = new Sprite(Texture)
+                    {
+                        Size = new Vector2(960, 540)
+                    };
+                    command = "";
+                }
+
+                RayaContext.Tick();
+
+                for (int x = 0; x < 960 / detail; x += 1)
+                {
+                    for (int y = 0; y < 540 / detail; y += 1)
+                    {
+                        int C = generateRandomNumber(0, 255);
+                        Image.SetPixel((uint)x, (uint)y, new Color(C, C, C));
+                    }
+                }
+
+                Texture.Update(Image);
+                fpsMeter.DisplayedString = RayaContext.FPS.ToString();
+
+
+                RayaContext.StartDraw();
+
+                RayaContext.Window.Draw(Sprite);
+                RayaContext.Window.Draw(fpsMeter);
+
+                RayaContext.EndDraw();
+            }
+        }
+
+        void ConsoleThread()
+        {
+            while (RayaContext.Running)
+            {
+                command = Console.ReadLine();
+            }
+        }
+
+        private static Random generator = new Random();
+        public static int generateRandomNumber(int Min = 0, int Max = 100)
+        {
+            return generator.Next(Min, Max + 1); //We add one because by Random.Next does not include max.
+        }
+
+        public static string command = "";
+    }
+}
