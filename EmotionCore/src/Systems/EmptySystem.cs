1cb8b591036de1391f076bce21b0873e303216f6
Vlad 'Cryru' Abadzhiev <TheCryru@gmail.com>
2018-03-23 02:25:50 +0200

    Build fix.

---
+++ EmotionCore/src/Systems/EmptySystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Emotion.Systems
+{
+    /// <summary>
+    /// Placeholder for namespace. When building in release mode the namespace is empty because the only system is the Debugging one.
+    /// </summary>
+    public class EmptySystem
+    {
+    }
+}
