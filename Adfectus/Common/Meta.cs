9f60337010a117b34b742c37f7828595eecb2c29
Vlad 'Cryru' Abadzhiev <TheCryru@gmail.com>
2019-04-03 00:00:09 +0300

    Nuget 0.0.11

--- Adfectus/Common/Meta.cs
+++ Adfectus/Common/Meta.cs
@@ -8,17 +8,17 @@
         /// <summary>
         /// The version of Adfectus.
         /// </summary>
-        public const string MajorVersion = "0.0.10";
+        public const string MajorVersion = "0.0.11";
 
         /// <summary>
         /// The commit hash for this version.
         /// </summary>
-        public const string CommitHash = "7334a02faf201c9f6d2abaeb4d446aec1c65e905";
+        public const string CommitHash = "0af3db8fc00a6c8cead87c433e1f6dd4054cf6e5";
 
         /// <summary>
         /// The message of the built commit.
         /// </summary>
-        public const string CommitMessage = "Added Coroutines and fixed frametime during non-fixed timesteps.";
+        public const string CommitMessage = "Fixed the render size changing the host size. Scripting improvements.";
 
         /// <summary>
         /// The full version string.
