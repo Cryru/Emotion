256701737b316a2098f0742930ea7452bf8a23d1
Vlad 'Cryru' Abadzhiev <TheCryru@gmail.com>
2019-11-06 22:31:40 +0200

    Added a command for executing arbitrary code within the render composer.

---
+++ Emotion/Graphics/Command/ExecCodeCommand.cs
@@ -0,0 +1,31 @@
+﻿#region Using
+
+#endregion
+
+using System;
+
+namespace Emotion.Graphics.Command
+{
+    /// <summary>
+    /// Executes code.
+    /// </summary>
+    public class ExecCodeCommand : RecyclableCommand
+    {
+        public Action Func;
+
+        public override void Process()
+        {
+            
+        }
+
+        public override void Execute(RenderComposer _)
+        {
+            Func?.Invoke();
+        }
+
+        public override void Recycle()
+        {
+            Func = null;
+        }
+    }
+}
