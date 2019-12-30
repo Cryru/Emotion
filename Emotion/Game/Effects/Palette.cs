7aa588aba90de9bb425ddb2bb28bec954204aa9e
Vlad Abadzhiev <TheCryru@gmail.com>
2019-12-31 01:08:24 +0200

    Forgotten function removed

--- Emotion/Game/Effects/Palette.cs
+++ Emotion/Game/Effects/Palette.cs
@@ -22,21 +22,5 @@ namespace Emotion.Game.Effects
             Name = name;
             Colors = colors;
         }
-
-        /// <summary>
-        /// Upload the palette to the shader.
-        /// </summary>
-        /// <param name="shader">The shader to upload to.</param>
-        /// <param name="uniformName">The name of the uniform</param>
-        public void Upload(ShaderProgram shader, string uniformName = "Palette")
-        {
-            var array = new Vector4[Colors.Length];
-            for (var i = 0; i < Colors.Length; i++)
-            {
-                array[i] = Colors[i].ToVec4();
-            }
-
-            shader.SetUniformVector4Array(uniformName, array);
-        }
     }
 }
