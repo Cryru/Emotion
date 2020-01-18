24e8a49a55555b8b314d220ae56fd9d3199ba445
Vlad Abadzhiev <TheCryru@gmail.com>
2020-01-18 20:58:17 +0200

    Engine.TotalTime will now be recorded in logs. Actual DateTime will no longer be.

--- Emotion/Standard/Logging/DefaultLogger.cs
+++ Emotion/Standard/Logging/DefaultLogger.cs
@@ -4,6 +4,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using Emotion.Common;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
@@ -31,11 +32,12 @@ namespace Emotion.Standard.Logging
         public DefaultLogger(bool stdOut, string logFolder = "Logs")
         {
             string fileName = $".{Path.DirectorySeparatorChar}{logFolder}{Path.DirectorySeparatorChar}{DateTime.Now:MM-dd-yyyy_HH-mm-ss}.log";
+            var template = "[{Level:u3}] {Message:lj}{NewLine}{Exception}";
 
             LoggerConfiguration loggerConfig = new LoggerConfiguration()
                 .MinimumLevel.Is(LogEventLevel.Debug)
                 // Limit to 10 files, and maximum of 100mb per file.
-                .WriteTo.Async(a => a.File(fileName, fileSizeLimitBytes: 10000000, retainedFileCountLimit: 10));
+                .WriteTo.Async(a => a.File(fileName, fileSizeLimitBytes: 10000000, retainedFileCountLimit: 10, outputTemplate: template));
 
             // Keep only the last 10 logs.
             string[] fileCount = Directory.GetFiles(logFolder);
@@ -57,7 +59,7 @@ namespace Emotion.Standard.Logging
                 }
             }
 
-            if (stdOut) loggerConfig.WriteTo.Console(LogEventLevel.Verbose, theme: AnsiConsoleTheme.Code, outputTemplate: "[{Level:u3}] {Message:lj}{NewLine}{Exception}");
+            if (stdOut) loggerConfig.WriteTo.Console(LogEventLevel.Verbose, theme: AnsiConsoleTheme.Code, outputTemplate: template);
 
             _logger = loggerConfig.CreateLogger();
         }
@@ -65,7 +67,7 @@ namespace Emotion.Standard.Logging
         /// <inheritdoc />
         public override void Log(MessageType type, string source, string message)
         {
-            string fullMessage = $"[{source}] [{Thread.CurrentThread.Name}/{Thread.CurrentThread.ManagedThreadId}] {message}";
+            string fullMessage = $"{Engine.TotalTime} [{source}] [{Thread.CurrentThread.Name}/{Thread.CurrentThread.ManagedThreadId}] {message}";
 
             switch (type)
             {
