a9b5666d5342d3818c3bd481e1ed8a85246cdf89
Vlad Abadzhiev <TheCryru@gmail.com>
2020-02-04 22:48:35 +0200

    TextLayouterWrap will try to fit the text in the box in height as well.

--- Emotion/Game/Text/TextLayouterWrap.cs
+++ Emotion/Game/Text/TextLayouterWrap.cs
@@ -82,6 +82,9 @@ namespace Emotion.Game.Text
                     if(text[i] != '\n') _newLineIndices.Add(i); // The new line here is handled by the TextLayouter.
                     currentLine = "";
 
+                    // Check if exceeding height box.
+                    if(NeededHeight >= bounds.Y) break;
+
                     // If the current character is a new line break retroactively push it on the last line, and continue without adding it to the current line.
                     if (text[i] == '\n' && _newLineIndices.Count > 0)
                     {
