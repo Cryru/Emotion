332f9474cc237a074b625baf37419e7f5db9decc
Vlad Abadzhiev <TheCryru@gmail.com>
2020-02-17 23:26:14 +0200

    Tree is now IEnumerable

--- Emotion/Game/Tree.cs
+++ Emotion/Game/Tree.cs
@@ -1,5 +1,6 @@
 ﻿#region Using
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,13 +13,13 @@ namespace Emotion.Game
     /// </summary>
     /// <typeparam name="T">The data type representing the branch names.</typeparam>
     /// <typeparam name="T2">The data type representing the leaf values.</typeparam>
-    public class Tree<T, T2>
+    public class Tree<T, T2> : IEnumerable<T2>
     {
         public T Name { get; set; }
         public List<T2> Leaves { get; set; } = new List<T2>();
         public List<Tree<T, T2>> Branches { get; set; } = new List<Tree<T, T2>>();
 
-        public Tree(T name)
+        protected Tree(T name)
         {
             Name = name;
         }
@@ -48,6 +49,50 @@ namespace Emotion.Game
             }
 
             target.Leaves.Add(value);
+        }
+
+        private class IterationState
+        {
+            public Tree<T, T2> Branch;
+            public int BranchIdx;
+        }
+
+        /// <summary>
+        /// Coroutine for iterating the tree. The leaves of each node are returned as we iterate towards the deepest level and then backward.
+        /// </summary>
+        /// <param name="rootNode">The node to start from.</param>
+        /// <returns>The leaf values along the tree.</returns>
+        public IEnumerator<T2> TreeEnumeratorCoroutine(Tree<T, T2> rootNode)
+        {
+            var list = new List<IterationState>
+            {
+                new IterationState {Branch = rootNode, BranchIdx = 0}
+            };
+
+            while (list.Count > 0)
+            {
+                IterationState branch = list[^1];
+                Tree<T, T2> newBranch = branch.Branch.Branches[branch.BranchIdx];
+                foreach (T2 leaf in newBranch.Leaves)
+                {
+                    yield return leaf;
+                }
+
+                if (newBranch.Branches.Count > 0) list.Add(new IterationState {Branch = newBranch, BranchIdx = 0});
+
+                branch.BranchIdx++;
+                if (branch.BranchIdx == branch.Branch.Branches.Count) list.Remove(branch);
+            }
+        }
+
+        public IEnumerator<T2> GetEnumerator()
+        {
+            return TreeEnumeratorCoroutine(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
