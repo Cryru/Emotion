b50356de3cec21bb5e605f424147e4fba335f369
Vlad Abadzhiev <TheCryru@gmail.com>
2020-02-18 22:33:07 +0200

    Added an after timer which can be reversed.

---
+++ Emotion/Game/Time/AfterAndBack.cs
@@ -0,0 +1,58 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace Emotion.Game.Time
+{
+    public class AfterAndBack : After
+    {
+        public bool InReverse { get; protected set; }
+
+        public AfterAndBack(float delay, Action function = null) : base(delay, function)
+        {
+        }
+
+        public void GoInReverse()
+        {
+            Restart();
+            InReverse = true;
+            _timePassed = _delay;
+        }
+
+        public void GoNormal()
+        {
+            Restart();
+            InReverse = false;
+        }
+
+        public override void Update(float timePassed)
+        {
+            if (Finished) return;
+
+            if (!InReverse)
+            {
+                base.Update(timePassed);
+                return;
+            }
+
+            _timePassed -= timePassed;
+            if (_timePassed <= 0) End();
+        }
+
+        public override void End()
+        {
+            if (InReverse)
+            {
+                _timePassed = 0;
+            }
+            else
+            {
+                _timePassed = _delay;
+            }
+            _function?.Invoke();
+            Finished = true;
+        }
+    }
+}
