9410557bea6cfabfcbb3a8f7d999ab88a50a36a2
Vlad Abadzhiev <TheCryru@gmail.com>
2020-03-12 00:07:19 +0200

    Inlined extension methods and an extension for Vec2 -> Int Tuple and IntRound

--- Emotion/Utility/Extensions.cs
+++ Emotion/Utility/Extensions.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 
 #endregion
 
@@ -15,6 +16,7 @@ namespace Emotion.Utility
         /// <summary>
         /// Returns an inverted copy of this instance.
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix4x4 Inverted(this Matrix4x4 m)
         {
             Matrix4x4.Invert(m, out Matrix4x4 inv);
@@ -26,6 +28,7 @@ namespace Emotion.Utility
         /// </summary>
         /// <param name="v">The vector to convert.</param>
         /// <returns>The Vector3 as a Vector2, with the Z unit stripped.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 ToVec2(this Vector3 v)
         {
             return new Vector2(v.X, v.Y);
@@ -35,7 +38,9 @@ namespace Emotion.Utility
         /// Converts a vector2 to a vector3.
         /// </summary>
         /// <param name="v">The vector to convert.</param>
+        /// <param name="z">The z value to set to the new component.</param>
         /// <returns>The Vector2 as a Vector3, with the Z unit added.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 ToVec3(this Vector2 v, float z = 0)
         {
             return new Vector3(v.X, v.Y, z);
@@ -46,11 +51,42 @@ namespace Emotion.Utility
         /// </summary>
         /// <param name="v">The vector to convert.</param>
         /// <returns>The Vector4 as a Vector3, with the W unit stripped.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 ToVec3(this Vector4 v)
         {
             return new Vector3(v.X, v.Y, v.Z);
         }
 
+        /// <summary>
+        /// Round the vector by casting it's components to ints.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2 IntCastRound(this Vector2 v)
+        {
+            v.X = (int) v.X;
+            v.Y = (int) v.Y;
+            return v;
+        }
+
+        /// <summary>
+        /// Get the vector's components as an int tuple.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (int x, int y) AsInt(this Vector2 v)
+        {
+            return ((int) v.X, (int) v.Y);
+        }
+
+        /// <summary>
+        /// Round the vector's components down.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 Floor(this Vector3 v)
         {
             v.X = MathF.Floor(v.X);
@@ -59,6 +95,12 @@ namespace Emotion.Utility
             return v;
         }
 
+        /// <summary>
+        /// Round the vector's components down.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 Floor(this Vector2 v)
         {
             v.X = MathF.Floor(v.X);
@@ -66,6 +108,12 @@ namespace Emotion.Utility
             return v;
         }
 
+        /// <summary>
+        /// Round the vector's components up.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 Ceiling(this Vector3 v)
         {
             v.X = MathF.Ceiling(v.X);
@@ -74,6 +122,12 @@ namespace Emotion.Utility
             return v;
         }
 
+        /// <summary>
+        /// Round the vector's components up.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 Ceiling(this Vector2 v)
         {
             v.X = MathF.Ceiling(v.X);
@@ -81,6 +135,12 @@ namespace Emotion.Utility
             return v;
         }
 
+        /// <summary>
+        /// Round the vector's components using Maths.RoundClosest.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 RoundClosest(this Vector2 v)
         {
             v.X = Maths.RoundClosest(v.X);
@@ -88,6 +148,12 @@ namespace Emotion.Utility
             return v;
         }
 
+        /// <summary>
+        /// Round the vector's components using Maths.RoundClosest.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 RoundClosest(this Vector3 v)
         {
             v.X = Maths.RoundClosest(v.X);
@@ -96,16 +162,40 @@ namespace Emotion.Utility
             return v;
         }
 
+        /// <summary>
+        /// Returns whether the components of the second vector are all larger or equal to this vector's.
+        /// >=
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="comp"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool LargerOrEqual(this Vector2 v, Vector2 comp)
         {
             return v.X >= comp.X && v.Y >= comp.Y;
         }
 
+        /// <summary>
+        /// Returns whether the components of the second vector are all smaller or equal to this vector's.
+        /// Basically &lt;=
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="comp"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool SmallerOrEqual(this Vector2 v, Vector2 comp)
         {
             return v.X <= comp.X && v.Y <= comp.Y;
         }
 
+        /// <summary>
+        /// Adds an element to an array. This is worse than using a list and will resize the array.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T[] AddToArray<T>(this T[] array, T element)
         {
             Array.Resize(ref array, array.Length + 1);
@@ -113,6 +203,15 @@ namespace Emotion.Utility
             return array;
         }
 
+        /// <summary>
+        /// Returns the index of an element within an array, or -1 if not found.
+        /// This will loop the whole array.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int IndexOf<T>(this T[] array, T element)
         {
             for (var i = 0; i < array.Length; i++)
