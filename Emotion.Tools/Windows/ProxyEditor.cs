c9f34724b992252c4c9764d833f2ad4f34385ca6
Vlad Abadzhiev <TheCryru@gmail.com>
2020-10-25 19:56:32 +0200

    [Tools] You can now make ImGui editors by just passing in an update and draw function.
    Don't abuse this please.

---
+++ Emotion.Tools/Windows/ProxyEditor.cs
@@ -0,0 +1,32 @@
+﻿#region Using
+
+using System;
+using Emotion.Graphics;
+using Emotion.Plugins.ImGuiNet.Windowing;
+
+#endregion
+
+namespace Emotion.Tools.Windows
+{
+    public class ProxyEditor : ImGuiWindow
+    {
+        private Action _update;
+        private Action<RenderComposer> _render;
+
+        public ProxyEditor(string title, Action update, Action<RenderComposer> render) : base(title)
+        {
+            _update = update;
+            _render = render;
+        }
+
+        public override void Update()
+        {
+            _update?.Invoke();
+        }
+
+        protected override void RenderContent(RenderComposer composer)
+        {
+            _render?.Invoke(composer);
+        }
+    }
+}
