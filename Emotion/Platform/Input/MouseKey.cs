9ed7853987cb64a99a19672711e0270256c4fa3f
Vlad Abadzhiev <TheCryru@gmail.com>
2021-01-03 12:58:26 +0200

    [Input] Added more detailed axis support, and mouse key 4 and 5 support.
    Moving all key based input to the Key enum.
    You can now query axis directions individually.

--- Emotion/Platform/Input/MouseKey.cs
+++ Emotion/Platform/Input/MouseKey.cs
@@ -1,11 +1,13 @@
 ﻿namespace Emotion.Platform.Input
 {
-    public enum MouseKey
+    public enum MouseKey : short
     {
         Unknown,
         Left,
         Right,
         Middle,
+        Key4,
+        Key5,
         Last
     }
 }
