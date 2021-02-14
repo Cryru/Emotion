81084cc3e139408d8eaf0ca78ded766730681605
Vlad Abadzhiev <TheCryru@gmail.com>
2021-02-14 14:28:03 +0200

    [Game] Added a new ReasonStack class

---
+++ Emotion/Game/ReasonStack.cs
@@ -0,0 +1,32 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Emotion.Game
+{
+    public class ReasonStack
+    {
+        public event EventHandler OnChange;
+        private HashSet<string> _stack = new HashSet<string>();
+
+        public void AddReason(string reason)
+        {
+            _stack.Add(reason);
+            OnChange?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void RemoveReason(string reason)
+        {
+            _stack.Remove(reason);
+            OnChange?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool AnyReason()
+        {
+            return _stack.Count > 0;
+        }
+    }
+}
