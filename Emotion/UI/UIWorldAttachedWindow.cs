f85e2c47d71e8686803cf99796ad8255d8b8bd72
Vlad Abadzhiev <TheCryru@gmail.com>
2021-10-17 19:27:27 +0300

    [UI] Added a basic UI window for attaching UI to world objects.

---
+++ Emotion/UI/UIWorldAttachedWindow.cs
@@ -0,0 +1,53 @@
+﻿#region Using
+
+using System.Numerics;
+using Emotion.Graphics;
+
+#endregion
+
+namespace Emotion.UI
+{
+    public class UIWorldAttachedWindow : UIBaseWindow
+    {
+        protected Vector3 _worldPos;
+        protected bool _awaitingLayout = true;
+
+        public UIWorldAttachedWindow()
+        {
+            StretchX = true;
+            StretchY = true;
+        }
+
+        public void AttachToPosition(Vector3 pos)
+        {
+            _worldPos = pos;
+        }
+
+        public override void InvalidateLayout()
+        {
+            _awaitingLayout = true;
+            base.InvalidateLayout();
+        }
+
+        protected override void AfterLayout()
+        {
+            _awaitingLayout = false;
+            base.AfterLayout();
+        }
+
+        protected override bool RenderInternal(RenderComposer c)
+        {
+            if (Children == null || _awaitingLayout) return false;
+
+            Vector3 pos = c.Camera.WorldToScreen(_worldPos.ToVec2()).ToVec3(_worldPos.Z);
+            c.PushModelMatrix(Matrix4x4.CreateTranslation(pos));
+            return base.RenderInternal(c);
+        }
+
+        protected override void AfterRenderChildren(RenderComposer c)
+        {
+            base.AfterRenderChildren(c);
+            c.PopModelMatrix();
+        }
+    }
+}
