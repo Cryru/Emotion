1f96e4097734ca22a8051eb0574f6d1dab5b95f9
Vlad Abadzhiev <TheCryru@gmail.com>
2021-10-24 16:20:41 +0300

    [Input] Deprecated KeyStatus.Held

--- Emotion/Platform/Input/KeyStatus.cs
+++ Emotion/Platform/Input/KeyStatus.cs
@@ -1,9 +1,13 @@
-﻿namespace Emotion.Platform.Input
+﻿using System;
+
+namespace Emotion.Platform.Input
 {
     public enum KeyStatus
     {
         Down,
         Up,
+        [Obsolete("The frequency and behavior of held events depend on support from the platform." +
+                  "\nInstead of using this key status consider a key that you have received DOWN for, but not UP as HELD")]
         Held
     }
 }
