a50258cc8459cb9c069b32935a36c080ed4a7a18
Vlad Abadzhiev <TheCryru@gmail.com>
2022-07-03 23:52:44 +0300

    [Tools] RenderDoc integration reworked.

---
+++ Emotion/Platform/Debugger/RenderDocAPI.cs
@@ -0,0 +1,63 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace Emotion.Platform.Debugger
+{
+#pragma warning disable 649 // Uninitialized field
+#pragma warning disable 0169 // Unused field
+#pragma warning disable IDE0044 // Add readonly modifier
+#pragma warning disable IDE0051 // Unused field
+
+    /// <summary>
+    /// RenderDoc Integration API
+    /// Holds references to the loaded API functions.
+    /// </summary>
+    public struct RenderDocAPI
+    {
+        public IntPtr GetAPIVersion;
+
+        public IntPtr SetCaptureOptionU32;
+        public IntPtr SetCaptureOptionF32;
+
+        public IntPtr GetCaptureOptionU32;
+        public IntPtr GetCaptureOptionF32;
+
+        public IntPtr SetFocusToggleKeys;
+        public IntPtr SetCaptureKeys;
+
+        public IntPtr GetOverlayBits;
+        public IntPtr MaskOverlayBits;
+
+        public IntPtr Shutdown;
+        public IntPtr UnloadCrashHandler;
+
+        public IntPtr SetCaptureFilePathTemplate;
+        public IntPtr GetCaptureFilePathTemplate;
+
+        public IntPtr GetNumCaptures;
+        public IntPtr GetCapture;
+
+        public IntPtr TriggerCapture;
+
+        public IntPtr IsTargetControlConnected;
+        public IntPtr LaunchReplayUI;
+
+        public IntPtr SetActiveWindow;
+
+        public IntPtr StartFrameCapture;
+        public IntPtr IsFrameCapturing;
+
+        public IntPtr EndFrameCapture;
+
+        public IntPtr TriggerMultiFrameCapture;
+
+        public delegate void CaptureFunc(IntPtr device, IntPtr window);
+    }
+#pragma warning restore 649
+#pragma warning restore 0169
+#pragma warning restore IDE0044
+#pragma warning restore IDE0051
+}
