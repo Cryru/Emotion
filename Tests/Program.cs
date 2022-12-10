d1ec0a69b733781941ba5e54d4d5452c7fc92984
Vlad Abadzhiev <TheCryru@gmail.com>
2022-12-10 14:43:04 +0200

    [Tests] Test fixes

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -4,9 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
+using Emotion.Audio;
 using Emotion.Common;
-using Emotion.Graphics.Text;
-using Emotion.IO;
 using Emotion.Test;
 using Tests.Results;
 
@@ -14,51 +13,52 @@ using Tests.Results;
 
 namespace Tests
 {
-    internal class Program
-    {
-        private static Dictionary<string, Action<Configurator>> _otherConfigs = new Dictionary<string, Action<Configurator>>
-        {
-            {"compat", c => { c.RendererCompatMode = true; }},
-            {
-                "tag=ResizeTest", c =>
-                {
-                    c.RenderSize = new Vector2(320, 180);
-                    c.UseIntermediaryBuffer = true;
-                }
-            },
-            {"tag=EmotionDesktop testOnly", null},
-            {
-                "tag=Assets", c =>
-                {
-                    // Cleanup for storage tests.
-                    if (!Directory.Exists("Player")) return;
-                    Directory.Delete("Player", true);
-                    Directory.CreateDirectory("Player");
-                }
-            },
-            {"tag=Scripting", null},
-            {"tag=Coroutine", null},
-            {"tag=StandardAudio", null},
-            {"tag=Audio", null},
-            {"tag=StandardText", null},
-            {"tag=AnimatedTexture", null},
-            {"tag=XML testOnly", null},
-            {"tag=UITests", null}
-        };
+	internal class Program
+	{
+		private static Dictionary<string, Action<Configurator>> _otherConfigs = new Dictionary<string, Action<Configurator>>
+		{
+			{"compat", c => { c.RendererCompatMode = true; }},
+			{
+				"tag=ResizeTest", c =>
+				{
+					c.RenderSize = new Vector2(320, 180);
+					c.UseIntermediaryBuffer = true;
+				}
+			},
+			{"tag=EmotionDesktop testOnly", null},
+			{
+				"tag=Assets", c =>
+				{
+					// Cleanup for storage tests.
+					if (!Directory.Exists("Player")) return;
+					Directory.Delete("Player", true);
+					Directory.CreateDirectory("Player");
+				}
+			},
+			{"tag=Scripting", null},
+			{"tag=Coroutine", null},
+			{"tag=StandardAudio", null},
+			{"tag=Audio", null},
+			{"tag=StandardText", null},
+			{"tag=AnimatedTexture", null},
+			{"tag=XML testOnly", null},
+			{"tag=UITests", null}
+		};
 
-        private static void Main(string[] args)
-        {
-            //FontAsset.GlyphRasterizer = GlyphRasterizer.StbTrueType;
-            ResultDb.LoadCache();
-            var config = new Configurator
-            {
-                HostSize = new Vector2(640, 360),
-                RenderSize = new Vector2(640, 360),
-                NoErrorPopup = true,
-                UseEmotionFontSize = true,
-                ExtraArgs = new [] { "software" }
-            };
-            Runner.RunTests(config, args, _otherConfigs, ResultDb.CachedResults);
-        }
-    }
+		private static void Main(string[] args)
+		{
+			//FontAsset.GlyphRasterizer = GlyphRasterizer.StbTrueType;
+			ResultDb.LoadCache();
+			var config = new Configurator
+			{
+				HostSize = new Vector2(640, 360),
+				RenderSize = new Vector2(640, 360),
+				NoErrorPopup = true,
+				UseEmotionFontSize = true,
+				AudioQuality = AudioResampleQuality.HighHann,
+				ExtraArgs = new[] {"software"} // Enable software renderer to ensure consistent results.
+			};
+			Runner.RunTests(config, args, _otherConfigs, ResultDb.CachedResults);
+		}
+	}
 }
