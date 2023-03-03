3efe0bedcb34159be28f30cd169f36d937cdfc64
Vlad Abadzhiev <TheCryru@gmail.com>
2023-03-03 16:23:45 +0200

    [Renderer] Added DbgAddLine

--- Emotion/Graphics/Renderer.DebugObjs.cs
+++ Emotion/Graphics/Renderer.DebugObjs.cs
@@ -15,11 +15,12 @@ namespace Emotion.Graphics
 	{
 		private List<Vector3>? _triangles;
 		private List<Mesh>? _spheres;
+		private List<(Vector3, Vector3)>? _lines;
 
 		public void DbgAddTriangle(Vector3 p1, Vector3 p2, Vector3 p3)
 		{
 			if (!Engine.Configuration.DebugMode) return;
-			_triangles ??= new List<Vector3>();
+			_triangles ??= new();
 			_triangles.Add(p1);
 			_triangles.Add(p2);
 			_triangles.Add(p3);
@@ -30,14 +31,23 @@ namespace Emotion.Graphics
 			if (!Engine.Configuration.DebugMode) return;
 
 			var meshGen = new SphereMeshGenerator();
-			_spheres ??= new List<Mesh>();
-			_spheres.Add(meshGen.GenerateMesh().TransformMeshVertices(Matrix4x4.CreateTranslation(p)));
+			_spheres ??= new();
+			_spheres.Add(meshGen.GenerateMesh().TransformMeshVertices(Matrix4x4.CreateTranslation(p)).ColorMeshVertices(Color.Green * 0.5f));
+		}
+
+		public void DbgAddLine(Vector3 p, Vector3 p2, bool relative = false)
+		{
+			if (!Engine.Configuration.DebugMode) return;
+
+			_lines ??= new();
+			_lines.Add((p, relative ? p2 + p : p2));
 		}
 
 		public void DbgClear()
 		{
 			_triangles?.Clear();
 			_spheres?.Clear();
+			_lines?.Clear();
 		}
 
 		private void RenderDebugObjects()
@@ -48,7 +58,7 @@ namespace Emotion.Graphics
 				for (var i = 0; i < memory.Length; i++)
 				{
 					memory[i].Vertex = _triangles[i];
-					memory[i].Color = Color.WhiteUint;
+					memory[i].Color = (Color.Green * 0.5f).ToUint();
 					memory[i].UV = Vector2.Zero;
 				}
 			}
@@ -58,6 +68,15 @@ namespace Emotion.Graphics
 				for (var i = 0; i < _spheres.Count; i++)
 				{
 					_spheres[i].Render(this);
+				}
+			}
+
+			if (_lines != null && _lines.Count > 0)
+			{
+				for (int i = 0; i < _lines.Count; i++)
+				{
+					var line = _lines[i];
+					RenderLine(line.Item1, line.Item2, Color.Green * 0.5f, 1, false);
 				}
 			}
 		}
