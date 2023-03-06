7c15a1f641fe7111654aeb5aaa25bbf89517f5c5
Vlad Abadzhiev <TheCryru@gmail.com>
2023-03-06 22:35:55 +0200

    [Android] Fixed OnConfigurationChanged rebuilds

--- Emotion.Android.ExecTest/MainActivity.cs
+++ Emotion.Android.ExecTest/MainActivity.cs
@@ -1,12 +1,20 @@
 #region Using
 
+using Android.Content.PM;
+using Android.Content.Res;
 using Emotion.Common;
+using Emotion.ExecTest;
 
 #endregion
 
 namespace Emotion.Android.ExecTest
 {
-	[Activity(Label = "@string/app_name", MainLauncher = true, HardwareAccelerated = true)]
+	[Activity(Label = "Android Test", MainLauncher = true, HardwareAccelerated = true,
+		ConfigurationChanges = ConfigChanges.Navigation | ConfigChanges.Orientation | ConfigChanges.LayoutDirection | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout |
+		                       ConfigChanges.ColorMode | ConfigChanges.Density | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.Mcc | ConfigChanges.Mnc |
+		                       ConfigChanges.Touchscreen | ConfigChanges.FontScale | ConfigChanges.FontWeightAdjustment | ConfigChanges.SmallestScreenSize | ConfigChanges.UiMode |
+		                       ConfigChanges.Locale
+	)]
 	public class MainActivity : Activity
 	{
 		public AndroidHost? Host;
@@ -20,13 +28,18 @@ namespace Emotion.Android.ExecTest
 			// Therefore we pass a callback to the host which will be executed on the GL surface creation.
 			Host = new AndroidHost(this, config =>
 			{
-				config.HostTitle = "Android Test";
 				config.DebugMode = true;
 				config.GlDebugMode = true;
 				Engine.Setup(config);
-				Engine.SceneManager.SetScene(new Emotion.ExecTest.Program());
+				Engine.SceneManager.SetScene(new Program());
 				Engine.Run();
 			});
+		}
+
+		public override void OnConfigurationChanged(Configuration newConfig)
+		{
+			base.OnConfigurationChanged(newConfig);
+			Engine.Log.Warning("Configuration changed!", "Android");
 		}
 
 		protected override void OnPause()
