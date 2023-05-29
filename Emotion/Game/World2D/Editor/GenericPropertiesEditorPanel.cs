b4e3bd9f4774203e64f91eeb23865180e9c81d52
Vlad Abadzhiev <TheCryru@gmail.com>
2023-05-30 01:43:13 +0300

    [World2D] Added generic properties editor

---
+++ Emotion/Game/World2D/Editor/GenericPropertiesEditorPanel.cs
@@ -0,0 +1,175 @@
+﻿#region Using
+
+using Emotion.Game.World2D.EditorHelpers;
+using Emotion.Standard.XML;
+using Emotion.UI;
+
+#endregion
+
+namespace Emotion.Game.World2D.Editor;
+
+public class GenericPropertiesEditorPanel : MapEditorPanel
+{
+	protected List<EditorUtility.TypeAndFieldHandlers> _fields;
+	protected List<IMapEditorGeneric> _editorUIs;
+
+	protected object _obj;
+
+	public GenericPropertiesEditorPanel(object obj) : base($"{obj} Properties")
+	{
+		_obj = obj;
+		_fields = EditorUtility.GetTypeFields(obj);
+		_editorUIs = new();
+	}
+
+	protected virtual void AddHeaderUI(UIBaseWindow uiContainer)
+	{
+	}
+
+	public override void AttachedToController(UIController controller)
+	{
+		base.AttachedToController(controller);
+
+		UIBaseWindow contentWin = _contentParent;
+		contentWin.InputTransparent = false;
+
+		var innerContainer = new UIBaseWindow();
+		innerContainer.StretchX = true;
+		innerContainer.StretchY = true;
+		innerContainer.InputTransparent = false;
+		innerContainer.LayoutMode = LayoutMode.VerticalList;
+		innerContainer.ListSpacing = new Vector2(0, 3);
+		innerContainer.ChildrenAllSameWidth = true;
+		contentWin.AddChild(innerContainer);
+
+		AddHeaderUI(innerContainer);
+
+		var listContainer = new UIBaseWindow();
+		listContainer.StretchX = true;
+		listContainer.StretchY = true;
+		listContainer.InputTransparent = false;
+		innerContainer.AddChild(listContainer);
+
+		var listNav = new UICallbackListNavigator();
+		listNav.LayoutMode = LayoutMode.VerticalList;
+		listNav.StretchX = true;
+		listNav.ListSpacing = new Vector2(0, 1);
+		listNav.Margins = new Rectangle(0, 0, 10, 0);
+		listNav.InputTransparent = false;
+		listNav.ChildrenAllSameWidth = true;
+		listContainer.AddChild(listNav);
+
+		var scrollBar = new UIScrollbar();
+		scrollBar.DefaultSelectorColor = MapEditorColorPalette.ButtonColor;
+		scrollBar.SelectorMouseInColor = MapEditorColorPalette.ActiveButtonColor;
+		scrollBar.WindowColor = Color.Black * 0.5f;
+		scrollBar.Anchor = UIAnchor.TopRight;
+		scrollBar.ParentAnchor = UIAnchor.TopRight;
+		scrollBar.MinSize = new Vector2(5, 0);
+		scrollBar.MaxSize = new Vector2(5, 9999);
+		listNav.SetScrollbar(scrollBar);
+		listContainer.AddChild(scrollBar);
+
+		// For each group of fields (inherited from the same class)
+		for (var i = 0; i < _fields.Count; i++)
+		{
+			EditorUtility.TypeAndFieldHandlers fieldGroup = _fields[i];
+
+			var fieldGroupHeaderContainer = new UIBaseWindow();
+			fieldGroupHeaderContainer.InputTransparent = false;
+			fieldGroupHeaderContainer.StretchX = true;
+			fieldGroupHeaderContainer.StretchY = true;
+
+			var fieldGroupHeader = new UIText();
+			fieldGroupHeader.ScaleMode = UIScaleMode.FloatScale;
+			fieldGroupHeader.WindowColor = MapEditorColorPalette.TextColor;
+			fieldGroupHeader.FontFile = "Editor/UbuntuMono-Regular.ttf";
+			fieldGroupHeader.FontSize = MapEditorColorPalette.EditorButtonTextSize + 1;
+			fieldGroupHeader.Underline = true;
+			fieldGroupHeader.IgnoreParentColor = true;
+			fieldGroupHeader.Text = fieldGroup.DeclaringType.Name;
+			fieldGroupHeader.MinSize = new Vector2(0, 11);
+			fieldGroupHeaderContainer.AddChild(fieldGroupHeader);
+
+			listNav.AddChild(fieldGroupHeaderContainer);
+
+			// For each field
+			for (var j = 0; j < fieldGroup.Fields.Count; j++)
+			{
+				XMLFieldHandler field = fieldGroup.Fields[j];
+
+				// todo: map editor readonly property
+				// todo: map editor hidden property (like uniqueId)
+				if (field.Name == "UniqueId") continue;
+
+				var fieldEditorContainer = new UIBaseWindow();
+				fieldEditorContainer.InputTransparent = false;
+				fieldEditorContainer.StretchX = true;
+				fieldEditorContainer.StretchY = true;
+				fieldEditorContainer.Margins = new Rectangle(3, 0, 0, 0);
+				fieldEditorContainer.LayoutMode = LayoutMode.HorizontalList;
+				fieldEditorContainer.ListSpacing = new Vector2(5, 0);
+
+				var label = new UIText();
+				label.ScaleMode = UIScaleMode.FloatScale;
+				label.WindowColor = MapEditorColorPalette.TextColor;
+				label.FontFile = "Editor/UbuntuMono-Regular.ttf";
+				label.FontSize = MapEditorColorPalette.EditorButtonTextSize;
+				label.IgnoreParentColor = true;
+				label.Anchor = UIAnchor.CenterLeft;
+				label.ParentAnchor = UIAnchor.CenterLeft;
+				label.Text = field.Name + ": ";
+				fieldEditorContainer.AddChild(label);
+
+				IMapEditorGeneric? editor = AddEditorForField(field);
+				if (editor != null)
+				{
+					editor.Field = field;
+					editor.SetCallbackValueChanged(newValue => { ApplyObjectChange(field, newValue); });
+
+					var editorAsWnd = (UIBaseWindow) editor;
+					editorAsWnd.Anchor = UIAnchor.CenterRight;
+					editorAsWnd.ParentAnchor = UIAnchor.CenterRight;
+					fieldEditorContainer.AddChild(editorAsWnd);
+
+					_editorUIs.Add(editor);
+				}
+
+				listNav.AddChild(fieldEditorContainer);
+			}
+		}
+
+		UpdatePropertyValues();
+	}
+
+	private IMapEditorGeneric? AddEditorForField(XMLFieldHandler field)
+	{
+		if (field.TypeHandler.Type == typeof(Vector2)) return new MapEditorFloat2();
+		if (field.TypeHandler.Type == typeof(float)) return new MapEditorNumber<float>();
+		if (field.TypeHandler.Type == typeof(int)) return new MapEditorNumber<int>();
+		if (field.TypeHandler.Type == typeof(Vector3)) return new MapEditorFloat3();
+		if (field.TypeHandler.Type == typeof(string)) return new MapEditorString();
+		if (field.TypeHandler.Type.IsEnum) return new MapEditorEnum(field.TypeHandler.Type);
+
+		return null;
+	}
+
+	protected virtual void ApplyObjectChange(XMLFieldHandler field, object value)
+	{
+		field.ReflectionInfo.SetValue(_obj, value);
+	}
+
+	public virtual void UpdatePropertyValues()
+	{
+		Controller?.SetInputFocus(null);
+
+		for (var i = 0; i < _editorUIs.Count; i++)
+		{
+			IMapEditorGeneric editor = _editorUIs[i];
+
+			XMLFieldHandler? field = editor.Field;
+			object? propertyValue = field.ReflectionInfo.GetValue(_obj);
+			editor.SetValue(propertyValue);
+		}
+	}
+}
--- Emotion/Game/World2D/Editor/MapEditorObjectPropertiesPanel.cs
+++ Emotion/Game/World2D/Editor/MapEditorObjectPropertiesPanel.cs
@@ -1,279 +1,161 @@
 ﻿#region Using
 
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Text;
+using Emotion.Common.Serialization;
+using Emotion.Game.World2D.Editor;
 using Emotion.Game.World2D.EditorHelpers;
 using Emotion.Standard.XML;
+using Emotion.Standard.XML.TypeHandlers;
 using Emotion.UI;
 
 #endregion
 
 #nullable enable
 
-namespace Emotion.Game.World2D
+namespace Emotion.Game.World2D;
+
+public sealed class MapEditorObjectPropertiesPanel : GenericPropertiesEditorPanel
 {
-	public sealed class MapEditorObjectPropertiesPanel : MapEditorPanel
+	public GameObject2D Object;
+	public int ObjectUId;
+	public Map2D ObjectMap;
+
+	private Action<GameObject2D> _objectChangedCallback;
+
+	private bool _objectReferenceInvalidated;
+
+	public MapEditorObjectPropertiesPanel(GameObject2D obj, Action<GameObject2D> objectChangeCallback) : base(obj)
 	{
-		public GameObject2D Object;
-		public int ObjectUId;
-		public Map2D ObjectMap;
+		Object = obj;
+		ObjectUId = Object.UniqueId;
+		ObjectMap = Object.Map;
 
-		private Action<GameObject2D> _objectChangedCallback;
-		private List<EditorUtility.TypeAndFieldHandlers> _fields;
-		private List<IMapEditorGeneric> _editorUIs;
-
-		private bool _objectReferenceInvalidated;
-
-		public MapEditorObjectPropertiesPanel(GameObject2D obj, Action<GameObject2D> objectChangeCallback) : base("Properties")
+		// Inject the "Center" property of Transform.
+		// Since it isn't serialized it wont be returned, but its handy to have for objects.
+		// todo: Make this injection function generic? In the base class or w/e.
+		EditorUtility.TypeAndFieldHandlers? positionalType = _fields.FirstOrDefault(x => x.DeclaringType == typeof(Positional));
+		if (positionalType != null)
 		{
-			Object = obj;
-			ObjectUId = Object.UniqueId;
-			ObjectMap = Object.Map;
-
-			_objectChangedCallback = objectChangeCallback;
-			_fields = EditorUtility.GetTypeFields(obj);
-			_editorUIs = new();
+			XMLTypeHandler? v2Handler = XMLHelpers.GetTypeHandler(typeof(Vector2));
+			PropertyInfo? centerProp = typeof(Transform).GetProperty("Center");
+			if (centerProp != null) positionalType.Fields.Add(new XMLFieldHandler(new ReflectedMemberHandler(centerProp), v2Handler));
 		}
 
-		public override void AttachedToController(UIController controller)
+		_objectChangedCallback = objectChangeCallback;
+	}
+
+	protected override void AddHeaderUI(UIBaseWindow uiContainer)
+	{
+		var statusLabel = new UIText();
+		statusLabel.ScaleMode = UIScaleMode.FloatScale;
+		statusLabel.WindowColor = MapEditorColorPalette.TextColor;
+		statusLabel.FontFile = "Editor/UbuntuMono-Regular.ttf";
+		statusLabel.FontSize = MapEditorColorPalette.EditorButtonTextSize;
+
+		var metaText = new StringBuilder();
+		metaText.Append(Object.ObjectState.ToString());
+		ObjectFlags[] objectFlags = Enum.GetValues<ObjectFlags>();
+		for (var i = 0; i < objectFlags.Length; i++)
 		{
-			Header = $"{Object.ObjectName ?? "Object"} [{ObjectUId}] Properties";
-
-			base.AttachedToController(controller);
-
-			UIBaseWindow contentWin = _contentParent;
-			contentWin.InputTransparent = false;
-
-			var innerContainer = new UIBaseWindow();
-			innerContainer.StretchX = true;
-			innerContainer.StretchY = true;
-			innerContainer.InputTransparent = false;
-			innerContainer.LayoutMode = LayoutMode.VerticalList;
-			innerContainer.ListSpacing = new Vector2(0, 3);
-			innerContainer.ChildrenAllSameWidth = true;
-			contentWin.AddChild(innerContainer);
-
-			var statusLabel = new UIText();
-			statusLabel.ScaleMode = UIScaleMode.FloatScale;
-			statusLabel.WindowColor = MapEditorColorPalette.TextColor;
-			statusLabel.FontFile = "Editor/UbuntuMono-Regular.ttf";
-			statusLabel.FontSize = MapEditorColorPalette.EditorButtonTextSize;
-
-			var metaText = new StringBuilder();
-			metaText.Append(Object.ObjectState.ToString());
-			ObjectFlags[] objectFlags = Enum.GetValues<ObjectFlags>();
-			for (var i = 0; i < objectFlags.Length; i++)
-			{
-				ObjectFlags flag = objectFlags[i];
-				if (flag == ObjectFlags.None) continue; // All have this :P
-				if (Object.ObjectFlags.HasFlag(flag)) metaText.Append($", {flag}");
-			}
-
-			// Warning: These objects might not actually be in these layers.
-			// If they reported a different value to IsPartOfMapLayer when being added.
-			metaText.Append("\nIn Layers: ");
-			var idx = 0;
-			foreach (int treeLayerId in ObjectMap.GetWorldTree()!.ForEachLayer())
-			{
-				if (Object.IsPartOfMapLayer(treeLayerId))
-				{
-					if (idx != 0) metaText.Append(", ");
-					metaText.Append(treeLayerId);
-					idx++;
-				}
-			}
-
-			statusLabel.Text = metaText.ToString();
-
-			innerContainer.AddChild(statusLabel);
-
-			var listContainer = new UIBaseWindow();
-			listContainer.StretchX = true;
-			listContainer.StretchY = true;
-			listContainer.InputTransparent = false;
-			innerContainer.AddChild(listContainer);
-
-			var listNav = new UICallbackListNavigator();
-			listNav.LayoutMode = LayoutMode.VerticalList;
-			listNav.StretchX = true;
-			listNav.ListSpacing = new Vector2(0, 1);
-			listNav.Margins = new Rectangle(0, 0, 10, 0);
-			listNav.InputTransparent = false;
-			listNav.ChildrenAllSameWidth = true;
-			listContainer.AddChild(listNav);
-
-			var scrollBar = new UIScrollbar();
-			scrollBar.DefaultSelectorColor = MapEditorColorPalette.ButtonColor;
-			scrollBar.SelectorMouseInColor = MapEditorColorPalette.ActiveButtonColor;
-			scrollBar.WindowColor = Color.Black * 0.5f;
-			scrollBar.Anchor = UIAnchor.TopRight;
-			scrollBar.ParentAnchor = UIAnchor.TopRight;
-			scrollBar.MinSize = new Vector2(5, 0);
-			scrollBar.MaxSize = new Vector2(5, 9999);
-			listNav.SetScrollbar(scrollBar);
-			listContainer.AddChild(scrollBar);
-
-			// For each group of fields (inherited from the same class)
-			for (var i = 0; i < _fields.Count; i++)
-			{
-				EditorUtility.TypeAndFieldHandlers fieldGroup = _fields[i];
-
-				var fieldGroupHeaderContainer = new UIBaseWindow();
-				fieldGroupHeaderContainer.InputTransparent = false;
-				fieldGroupHeaderContainer.StretchX = true;
-				fieldGroupHeaderContainer.StretchY = true;
-
-				var fieldGroupHeader = new UIText();
-				fieldGroupHeader.ScaleMode = UIScaleMode.FloatScale;
-				fieldGroupHeader.WindowColor = MapEditorColorPalette.TextColor;
-				fieldGroupHeader.FontFile = "Editor/UbuntuMono-Regular.ttf";
-				fieldGroupHeader.FontSize = MapEditorColorPalette.EditorButtonTextSize + 1;
-				fieldGroupHeader.Underline = true;
-				fieldGroupHeader.IgnoreParentColor = true;
-				fieldGroupHeader.Text = fieldGroup.DeclaringType.Name;
-				fieldGroupHeader.MinSize = new Vector2(0, 11);
-				fieldGroupHeaderContainer.AddChild(fieldGroupHeader);
-
-				listNav.AddChild(fieldGroupHeaderContainer);
-
-				// For each field
-				for (var j = 0; j < fieldGroup.Fields.Count; j++)
-				{
-					XMLFieldHandler field = fieldGroup.Fields[j];
-
-					// todo: map editor readonly property
-					// todo: map editor hidden property (like uniqueId)
-					if (field.Name == "UniqueId") continue;
-
-					var fieldEditorContainer = new UIBaseWindow();
-					fieldEditorContainer.InputTransparent = false;
-					fieldEditorContainer.StretchX = true;
-					fieldEditorContainer.StretchY = true;
-					fieldEditorContainer.Margins = new Rectangle(3, 0, 0, 0);
-					fieldEditorContainer.LayoutMode = LayoutMode.HorizontalList;
-					fieldEditorContainer.ListSpacing = new Vector2(5, 0);
-
-					var label = new UIText();
-					label.ScaleMode = UIScaleMode.FloatScale;
-					label.WindowColor = MapEditorColorPalette.TextColor;
-					label.FontFile = "Editor/UbuntuMono-Regular.ttf";
-					label.FontSize = MapEditorColorPalette.EditorButtonTextSize;
-					label.IgnoreParentColor = true;
-					label.Anchor = UIAnchor.CenterLeft;
-					label.ParentAnchor = UIAnchor.CenterLeft;
-					label.Text = field.Name + ": ";
-					fieldEditorContainer.AddChild(label);
-
-					IMapEditorGeneric? editor = AddEditorForField(field);
-					if (editor != null)
-					{
-						editor.Field = field;
-						editor.SetCallbackValueChanged(newValue => { ApplyObjectChange(field, newValue); });
-
-						var editorAsWnd = (UIBaseWindow) editor;
-						editorAsWnd.Anchor = UIAnchor.CenterRight;
-						editorAsWnd.ParentAnchor = UIAnchor.CenterRight;
-						fieldEditorContainer.AddChild(editorAsWnd);
-
-						_editorUIs.Add(editor);
-					}
-
-					listNav.AddChild(fieldEditorContainer);
-				}
-			}
-
-			UpdatePropertyValues();
+			ObjectFlags flag = objectFlags[i];
+			if (flag == ObjectFlags.None) continue; // All have this :P
+			if (Object.ObjectFlags.HasFlag(flag)) metaText.Append($", {flag}");
 		}
 
-		protected override bool UpdateInternal()
+		// Warning: These objects might not actually be in these layers.
+		// If they reported a different value to IsPartOfMapLayer when being added.
+		metaText.Append("\nIn Layers: ");
+		var idx = 0;
+		foreach (int treeLayerId in ObjectMap.GetWorldTree()!.ForEachLayer())
 		{
-			if (_objectReferenceInvalidated)
+			if (Object.IsPartOfMapLayer(treeLayerId))
 			{
-				ObjectReferenceUpdated();
-				_objectReferenceInvalidated = false;
-				return false;
-			}
-
-			return base.UpdateInternal();
-		}
-
-		private IMapEditorGeneric? AddEditorForField(XMLFieldHandler field)
-		{
-			if (field.TypeHandler.Type == typeof(Vector2)) return new MapEditorFloat2();
-			if (field.TypeHandler.Type == typeof(float)) return new MapEditorNumber<float>();
-			if (field.TypeHandler.Type == typeof(int)) return new MapEditorNumber<int>();
-			if (field.TypeHandler.Type == typeof(Vector3)) return new MapEditorFloat3();
-			if (field.TypeHandler.Type == typeof(string)) return new MapEditorString();
-			if (field.TypeHandler.Type.IsEnum) return new MapEditorEnum(field.TypeHandler.Type);
-
-			return null;
-		}
-
-		public void ApplyObjectChange(XMLFieldHandler field, object value)
-		{
-			GameObject2D oldObject = Object;
-			Map2D objectMap = oldObject.Map;
-
-			Debug.Assert(objectMap != null);
-
-			// Remove the object UndoAction and remove the object from the map.
-			_objectChangedCallback(Object);
-			objectMap.RemoveObject(Object, true);
-
-			// Serialize it and deserialize it to copy it.
-			string? objectAsData = XMLFormat.To(oldObject);
-			var newObject = XMLFormat.From<GameObject2D>(objectAsData);
-
-			// Change the property and add the object to map.
-			field.ReflectionInfo.SetValue(newObject, value);
-			objectMap.AddObject(newObject);
-
-			// Now the object has been copied with the changed property and re-added.
-			// This guarantees that objects dont have to handle properties changing after the init.
-		}
-
-		public void InvalidateObjectReference()
-		{
-			_objectReferenceInvalidated = true;
-		}
-
-		private void ObjectReferenceUpdated()
-		{
-			// Try to find the object we were showing.
-			GameObject2D? newObjectReference = null;
-			foreach (GameObject2D obj in ObjectMap.GetObjects())
-			{
-				if (obj.UniqueId == ObjectUId)
-				{
-					newObjectReference = obj;
-					break;
-				}
-			}
-
-			if (newObjectReference == null)
-			{
-				Controller?.RemoveChild(this);
-				return;
-			}
-
-			Debug.Assert(newObjectReference.GetType() == Object.GetType());
-			Object = newObjectReference;
-
-			UpdatePropertyValues();
-		}
-
-		private void UpdatePropertyValues()
-		{
-			Controller?.SetInputFocus(null);
-
-			for (var i = 0; i < _editorUIs.Count; i++)
-			{
-				IMapEditorGeneric editor = _editorUIs[i];
-
-				XMLFieldHandler? field = editor.Field;
-				object? propertyValue = field.ReflectionInfo.GetValue(Object);
-				editor.SetValue(propertyValue);
+				if (idx != 0) metaText.Append(", ");
+				metaText.Append(treeLayerId);
+				idx++;
 			}
 		}
+
+		statusLabel.Text = metaText.ToString();
+
+		uiContainer.AddChild(statusLabel);
+	}
+
+	protected override void ApplyObjectChange(XMLFieldHandler field, object value)
+	{
+		GameObject2D oldObject = Object;
+		Map2D objectMap = oldObject.Map;
+
+		Debug.Assert(objectMap != null);
+
+		// Remove the object UndoAction and remove the object from the map.
+		_objectChangedCallback(Object);
+		
+		
+		// Clean the project of unserialized properties.
+		// This basically brings it in line to how it will look when loaded the first time..
+		bool isPersist = Object.ObjectFlags.HasFlag(ObjectFlags.Persistent);
+		EditorUtility.SetObjectToSerializationDefault<GameObject2D>(Object);
+		if (isPersist) Object.ObjectFlags |= ObjectFlags.Persistent;
+
+		// Set the new value.
+		field.ReflectionInfo.SetValue(Object, value);
+
+		objectMap.Editor_ReinitializeObject(Object);
+	}
+
+	protected override bool UpdateInternal()
+	{
+		if (_objectReferenceInvalidated)
+		{
+			ObjectReferenceUpdated();
+			_objectReferenceInvalidated = false;
+			return false;
+		}
+
+		return base.UpdateInternal();
+	}
+
+	public override void UpdatePropertyValues()
+	{
+		base.UpdatePropertyValues();
+		Header = $"{Object.ObjectName ?? "Object"} [{ObjectUId}] Properties";
+	}
+
+	// todo: check wtf this is about
+	public void InvalidateObjectReference()
+	{
+		_objectReferenceInvalidated = true;
+	}
+
+	private void ObjectReferenceUpdated()
+	{
+		// Try to find the object we were showing.
+		GameObject2D? newObjectReference = null;
+		foreach (GameObject2D obj in ObjectMap.GetObjects())
+		{
+			if (obj.UniqueId == ObjectUId)
+			{
+				newObjectReference = obj;
+				break;
+			}
+		}
+
+		if (newObjectReference == null)
+		{
+			Controller?.RemoveChild(this);
+			return;
+		}
+
+		Debug.Assert(newObjectReference.GetType() == Object.GetType());
+		Debug.Assert(newObjectReference.UniqueId == Object.UniqueId);
+		Debug.Assert(newObjectReference.Map == ObjectMap);
+		Object = newObjectReference;
+		_obj = newObjectReference;
+
+		UpdatePropertyValues();
 	}
 }
