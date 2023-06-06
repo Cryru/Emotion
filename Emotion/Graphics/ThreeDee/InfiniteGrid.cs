f36b007cec1d84c44d4f3a685ea84283f6718866
Vlad Abadzhiev <TheCryru@gmail.com>
2023-06-06 23:08:24 +0300

    [3D] Added grid size option to InfiniteGrid

--- Emotion/Graphics/ThreeDee/InfiniteGrid.cs
+++ Emotion/Graphics/ThreeDee/InfiniteGrid.cs
@@ -4,34 +4,34 @@ using Emotion.IO;
 
 #endregion
 
-namespace Emotion.Graphics.ThreeDee
+#nullable enable
+
+namespace Emotion.Graphics.ThreeDee;
+
+public sealed class InfiniteGrid : Quad3D
 {
-	public sealed class InfiniteGrid : Quad3D
-	{
-		private ShaderAsset? _shader;
-		private Vector2 _infiniteGridSize = new Vector2(10_000, 10_000);
+    public float TileSize = 100;
 
-		public InfiniteGrid()
-		{
-			_shader = Engine.AssetLoader.Get<ShaderAsset>("Shaders/3DGrid.xml");
-			Size = _infiniteGridSize.ToVec3(1);
-		}
+    private ShaderAsset? _shader;
+    private Vector2 _infiniteGridSize = new Vector2(10_000, 10_000);
 
-		public void Update()
-		{
-		}
+    public InfiniteGrid()
+    {
+        _shader = Engine.AssetLoader.Get<ShaderAsset>("Shaders/3DGrid.xml");
+        Size = _infiniteGridSize.ToVec3(1);
+    }
 
-		public override void Render(RenderComposer c)
-		{
-			if (_shader == null) return;
+    public override void Render(RenderComposer c)
+    {
+        if (_shader == null) return;
 
-			Vector2 cameraPos = c.Camera.Position.ToVec2();
-			Position = cameraPos.ToVec3(Z); // Set position to camera position without the Z
-			c.SetShader(_shader.Shader);
-			_shader.Shader.SetUniformVector2("cameraPos", cameraPos / _infiniteGridSize); // Camera position in UV space.
-			_shader.Shader.SetUniformVector2("totalSize", _infiniteGridSize);
-			base.Render(c);
-			c.SetShader();
-		}
-	}
+        Vector2 cameraPos = c.Camera.Position.ToVec2();
+        Position = cameraPos.ToVec3(Z); // Set position to camera position without the Z
+        c.SetShader(_shader.Shader);
+        _shader.Shader.SetUniformVector2("squareSize", new Vector2(TileSize));
+        _shader.Shader.SetUniformVector2("cameraPos", cameraPos / _infiniteGridSize); // Camera position in UV space.
+        _shader.Shader.SetUniformVector2("totalSize", _infiniteGridSize);
+        base.Render(c);
+        c.SetShader();
+    }
 }
