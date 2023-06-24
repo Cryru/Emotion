373868607e1041294f517affaed8c416fce6f803
Vlad Abadzhiev <TheCryru@gmail.com>
2023-06-24 13:35:24 +0300

    [Serialization] Added GetAttribute to ReflectedMemberHandler

--- Emotion/Common/Serialization/ReflectedMemberHandler.cs
+++ Emotion/Common/Serialization/ReflectedMemberHandler.cs
@@ -1,76 +1,79 @@
 ﻿#region Using
 
-using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
 #endregion
 
-namespace Emotion.Common.Serialization
+#nullable enable
+
+namespace Emotion.Common.Serialization;
+
+/// <summary>
+/// Handles the interop between the serializers and C# reflection.
+/// </summary>
+public class ReflectedMemberHandler
 {
-    /// <summary>
-    /// Handles the interop between the serializers and C# reflection.
-    /// </summary>
-    public class ReflectedMemberHandler
-    {
-        /// <summary>
-        /// The name of the property this object handles.
-        /// </summary>
-        public string Name { get; }
+	/// <summary>
+	/// The name of the property this object handles.
+	/// </summary>
+	public string Name { get; }
 
-        /// <summary>
-        /// The type this member was declared in.
-        /// </summary>
-        public Type DeclaredIn
-        {
-            get => _prop?.DeclaringType ?? _field.DeclaringType;
-        }
+	/// <summary>
+	/// The type this member was declared in.
+	/// </summary>
+	public Type? DeclaredIn
+	{
+		get => _prop?.DeclaringType ?? _field?.DeclaringType ?? null;
+	}
 
-        private PropertyInfo _prop;
-        private FieldInfo _field;
+	private readonly PropertyInfo? _prop;
+	private readonly FieldInfo? _field;
 
-        /// <summary>
-        /// Create a new reflection handler for the specified property.
-        /// </summary>
-        /// <param name="prop"></param>
-        public ReflectedMemberHandler(PropertyInfo prop)
-        {
-            _prop = prop;
-            Name = _prop.Name;
-        }
+	/// <summary>
+	/// Create a new reflection handler for the specified property.
+	/// </summary>
+	/// <param name="prop"></param>
+	public ReflectedMemberHandler(PropertyInfo prop)
+	{
+		_prop = prop;
+		Name = _prop.Name;
+	}
 
-        /// <summary>
-        /// Create a new reflection handler for the specified field.
-        /// </summary>
-        /// <param name="field"></param>
-        public ReflectedMemberHandler(FieldInfo field)
-        {
-            _field = field;
-            Name = _field.Name;
-        }
+	/// <summary>
+	/// Create a new reflection handler for the specified field.
+	/// </summary>
+	/// <param name="field"></param>
+	public ReflectedMemberHandler(FieldInfo field)
+	{
+		_field = field;
+		Name = _field.Name;
+	}
 
-        /// <summary>
-        /// Get the value of the field or property this handler manages from the object instance.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public object GetValue(object obj)
-        {
-            if (_prop != null) return _prop.GetValue(obj);
-            return _field != null ? _field.GetValue(obj) : null;
-        }
+	/// <summary>
+	/// Get the value of the field or property this handler manages from the object instance.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public object? GetValue(object obj)
+	{
+		return _prop?.GetValue(obj) ?? _field?.GetValue(obj);
+	}
 
-        /// <summary>
-        /// Set the value of the field or property this handler manages from the object instance.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <param name="val"></param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void SetValue(object obj, object val)
-        {
-            if (_prop != null) _prop.SetValue(obj, val);
-            if (_field != null) _field.SetValue(obj, val);
-        }
-    }
+	/// <summary>
+	/// Set the value of the field or property this handler manages from the object instance.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void SetValue(object obj, object val)
+	{
+		_prop?.SetValue(obj, val);
+		_field?.SetValue(obj, val);
+	}
+
+	/// <summary>
+	/// Get an attribute from the reflected member, if any.
+	/// </summary>
+	public T? GetAttribute<T>() where T : Attribute
+	{
+		return _prop?.GetCustomAttribute<T>() ?? _field?.GetCustomAttribute<T>();
+	}
 }
