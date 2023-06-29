7b8248f35599747a6f806d38bec6ef234fefc330
Vlad Abadzhiev <TheCryru@gmail.com>
2023-06-30 00:04:07 +0300

    [Editor] Added animations dropdown to Model Viewer.

--- Emotion/Editor/EditorWindows/ModelViewer.cs
+++ Emotion/Editor/EditorWindows/ModelViewer.cs
@@ -1,9 +1,11 @@
 ﻿#region Using
 
+using System.Threading.Tasks;
 using Emotion.Common.Threading;
 using Emotion.Editor.EditorHelpers;
 using Emotion.Editor.EditorWindows.ModelViewerUtil;
 using Emotion.Editor.PropertyEditors;
+using Emotion.Game.Animation3D;
 using Emotion.Game.ThreeDee;
 using Emotion.Game.World2D.EditorHelpers;
 using Emotion.Graphics;
@@ -13,9 +15,9 @@ using Emotion.Graphics.Data;
 using Emotion.Graphics.Objects;
 using Emotion.Graphics.ThreeDee;
 using Emotion.IO;
+using Emotion.IO.MeshAssetTypes;
 using Emotion.Platform.Input;
 using Emotion.UI;
-using System.Threading.Tasks;
 
 #endregion
 
@@ -30,6 +32,14 @@ public class ModelViewer : MapEditorPanel
 	private FrameBuffer? _renderBuffer;
 
 	private UIBaseWindow? _surface3D;
+
+	private EditorDropDownButtonDescription[] _noAnimationItems =
+	{
+		new()
+		{
+			Name = "No Animation"
+		}
+	};
 
 	private bool _panelDragResize;
 
@@ -127,11 +137,31 @@ public class ModelViewer : MapEditorPanel
 		scaleEditor.SetCallbackValueChanged(newVal => { _obj.Size = (Vector3) newVal; });
 		editorButtons.AddChild(new FieldEditorWithLabel("Scale: ", scaleEditor, LayoutMode.VerticalList));
 
-		var meshListProp = new EditorCheckboxList("Meshes");
+		var meshListProp = new EditorCheckboxList("Meshes: ");
 		meshListProp.Id = "MeshList";
 		scaleEditor.SetValue(_obj.Size);
 		scaleEditor.SetCallbackValueChanged(newVal => { _obj.Size = (Vector3) newVal; });
 		editorButtons.AddChild(meshListProp);
+
+		var animationsList = new EditorButtonDropDown();
+		animationsList.Id = "Animations";
+		animationsList.Text = "Animation: ";
+		animationsList.LayoutMode = LayoutMode.VerticalList;
+		animationsList.SetItems(_noAnimationItems, 0);
+		_noAnimationItems[0].Click = SetAnimationDropDownCallback;
+		editorButtons.AddChild(animationsList);
+
+		var saveAsEm3Button = new MapEditorTopBarButton();
+		saveAsEm3Button.Text = "Export as Em3 (WIP)";
+		saveAsEm3Button.StretchY = true;
+		saveAsEm3Button.StretchX = false;
+		saveAsEm3Button.OnClickedProxy = _ =>
+		{
+			if (_obj.Entity == null) return;
+			byte[]? data = EmotionMeshAsset.EntityToByteArray(_obj.Entity);
+			Engine.AssetLoader.Save(data, $"Player/converted_{_obj.Entity.Name}.em3");
+		};
+		editorButtons.AddChild(saveAsEm3Button);
 
 		contentSplit.AddChild(editorButtons);
 		_contentParent.AddChild(contentSplit);
@@ -168,6 +198,38 @@ public class ModelViewer : MapEditorPanel
 			meshList.SetItems(MeshVisibleCheckboxListItem.CreateItemsFromObject3D(_obj));
 			meshList.Text = entity == null ? "Meshes" : $"Meshes [{entity.Meshes.Length}]";
 		}
+
+		var animationList = (EditorButtonDropDown?) GetWindowById("Animations");
+		if (animationList != null)
+		{
+			SkeletalAnimation[]? animations = entity?.Animations;
+			if (animations != null)
+			{
+				var animationButtons = new EditorDropDownButtonDescription[animations.Length + 1];
+				animationButtons[0] = _noAnimationItems[0];
+				for (var i = 0; i < animations.Length; i++)
+				{
+					SkeletalAnimation anim = animations[i];
+					animationButtons[i + 1] = new EditorDropDownButtonDescription
+					{
+						Name = anim.Name,
+						UserData = anim.Name,
+						Click = SetAnimationDropDownCallback
+					};
+				}
+
+				animationList.SetItems(animationButtons, 0);
+			}
+			else
+			{
+				animationList.SetItems(_noAnimationItems, 0);
+			}
+		}
+	}
+
+	protected void SetAnimationDropDownCallback(EditorDropDownButtonDescription item, MapEditorTopBarButton _)
+	{
+		_obj.SetAnimation(item.Name);
 	}
 
 	public override void DetachedFromController(UIController controller)
@@ -188,6 +250,7 @@ public class ModelViewer : MapEditorPanel
 	protected override bool UpdateInternal()
 	{
 		_camera.Update();
+		_obj.Update(Engine.DeltaTime);
 
 		if (_panelDragResize && _surface3D != null)
 		{
@@ -226,13 +289,9 @@ public class ModelViewer : MapEditorPanel
 		c.RenderLine(new Vector3(0, 0, 0), new Vector3(0, 0, short.MaxValue), Color.Blue, snapToPixel: false);
 
 		if (_obj.Entity?.AnimationRig != null && _boneVerticesStream != null)
-		{
 			_obj.RenderAnimated(c, _boneVerticesStream);
-		}
 		else
-		{
 			_obj.Render(c);
-		}
 
 		c.RenderTo(null);
 		_boneVerticesStream?.DoTasks(c);
