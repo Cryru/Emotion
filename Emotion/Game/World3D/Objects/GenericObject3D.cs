78ab4aa816e5c2325176b7f03f6ad892b86881bd
Vlad Abadzhiev <TheCryru@gmail.com>
2023-09-20 09:38:44 +0300

    [3D] Moved EntityPath from GameObject3D to GenericObject3D

--- Emotion/Game/World3D/Objects/GenericObject3D.cs
+++ Emotion/Game/World3D/Objects/GenericObject3D.cs
@@ -1,8 +1,26 @@
-﻿namespace Emotion.Game.World3D.Objects;
+﻿#nullable enable
+
+#region Using
+
+using System.Threading.Tasks;
+using Emotion.Editor;
+using Emotion.IO;
+
+#endregion
+
+namespace Emotion.Game.World3D.Objects;
 
 public class GenericObject3D : GameObject3D
 {
-	public GenericObject3D()
+	[AssetFileName<MeshAsset>]
+	public string? EntityPath;
+
+	public override async Task LoadAssetsAsync()
 	{
+		await base.LoadAssetsAsync();
+		if (string.IsNullOrEmpty(EntityPath)) return;
+
+		var asset = await Engine.AssetLoader.GetAsync<MeshAsset>(EntityPath);
+		Entity = asset?.Entity;
 	}
 }
