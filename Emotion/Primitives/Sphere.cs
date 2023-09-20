4ece30b0321415962a1913b0ccc14a0cb08cd84c
Vlad Abadzhiev <TheCryru@gmail.com>
2023-09-20 09:43:00 +0300

    [Primitives] Small optimization to Sphere matrix transformation

--- Emotion/Primitives/Sphere.cs
+++ Emotion/Primitives/Sphere.cs
@@ -15,10 +15,12 @@ public struct Sphere
 
 	public Sphere Transform(Matrix4x4 mat)
 	{
-		if (!Matrix4x4.Decompose(mat, out Vector3 scale, out Quaternion rot, out Vector3 trans)) return this;
-
 		Vector3 transformedOrigin = Vector3.Transform(Origin, mat);
-		float maxScale = Math.Max(Math.Max(scale.X, scale.Y), scale.Z);
+
+		float scaleX = new Vector3(mat.M11, mat.M21, mat.M31).Length();
+		float scaleY = new Vector3(mat.M12, mat.M22, mat.M32).Length();
+		float scaleZ = new Vector3(mat.M13, mat.M23, mat.M33).Length();
+		float maxScale = Math.Max(Math.Max(scaleX, scaleY), scaleZ);
 		float transformedRadius = Radius * maxScale;
 		return new Sphere(transformedOrigin, transformedRadius);
 	}
