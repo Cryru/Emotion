e3001ab040a0c6c33a3c18ecb25fc5612dddc0e6
Vlad Abadzhiev <TheCryru@gmail.com>
2023-09-23 13:32:22 +0300

    Build Fix

--- Emotion/Editor/PropertyEditors/PropEditorArray.cs
+++ Emotion/Editor/PropertyEditors/PropEditorArray.cs
@@ -3,13 +3,8 @@
 #region Using
 
 using Emotion.Editor.EditorHelpers;
-using Emotion.Game.Animation3D;
-using Emotion.Game.World3D;
 using Emotion.Standard.XML;
 using Emotion.UI;
-using Emotion.Utility;
-using Silk.NET.Assimp;
-using System.Runtime.InteropServices;
 
 #endregion
 
@@ -17,73 +12,74 @@ namespace Emotion.Editor.PropertyEditors;
 
 public class PropEditorArray : EditorButtonDropDown, IPropEditorGeneric
 {
-    public XMLFieldHandler Field { get; set; }
+	public XMLFieldHandler Field { get; set; }
 
-    private EditorDropDownButtonDescription[] _noItems =
-    {
-            new()
-            {
-                Name = "No Items",
-            }
-        };
+	private EditorDropDownButtonDescription[] _noItems =
+	{
+		new()
+		{
+			Name = "No Items",
+		}
+	};
 
-    private object _value;
-    private Action<object> _changeCallback;
+	private object _value;
+	private Action<object> _changeCallback;
 
-    public override void AttachedToController(UIController controller)
-    {
-        base.AttachedToController(controller);
-        FillDropdownItems();
-    }
+	public override void AttachedToController(UIController controller)
+	{
+		base.AttachedToController(controller);
+		FillDropdownItems();
+	}
 
-    public void SetValue(object value)
-    {
-        _value = value;
-    }
+	public void SetValue(object value)
+	{
+		_value = value;
+	}
 
-    public object GetValue()
-    {
-        return _value;
-    }
+	public object GetValue()
+	{
+		return _value;
+	}
 
-    public void SetCallbackValueChanged(Action<object> callback)
-    {
-        _changeCallback = callback;
-    }
+	public void SetCallbackValueChanged(Action<object> callback)
+	{
+		_changeCallback = callback;
+	}
 
-    private void FillDropdownItems()
-    {
-        var valueAsArray = (Array?)_value;
-        if (valueAsArray != null)
-        {
-            var arrayItems = new EditorDropDownButtonDescription[valueAsArray.Length];
-            for (int i = 0; i < valueAsArray.Length; i++)
-            {
-                var value = valueAsArray.GetValue(i);
-                arrayItems[i] = new EditorDropDownButtonDescription
-                {
-                    Name = value?.ToString() ?? "<null>",
-                    Click = (_, __) =>
-                    {
-                        if (value == null) return;
-                        var panel = new GenericPropertiesEditorPanel(value);
-                        Controller?.AddChild(panel);
-                    }
-                };
-            }
-            SetItems(arrayItems, 0);
-        }
-        else
-        {
-            SetItems(_noItems, 0);
-        }
-    }
+	private void FillDropdownItems()
+	{
+		var valueAsArray = (Array?) _value;
+		if (valueAsArray != null)
+		{
+			var arrayItems = new EditorDropDownButtonDescription[valueAsArray.Length];
+			for (var i = 0; i < valueAsArray.Length; i++)
+			{
+				object? value = valueAsArray.GetValue(i);
+				arrayItems[i] = new EditorDropDownButtonDescription
+				{
+					Name = value?.ToString() ?? "<null>",
+					Click = (_, __) =>
+					{
+						if (value == null) return;
+						var panel = new GenericPropertiesEditorPanel(value);
+						Controller?.AddChild(panel);
+					}
+				};
+			}
 
-    protected override void UpdateCurrentOptionText()
-    {
-        var button = (EditorButton?)GetWindowById("Button");
-        if (button == null) return;
-        button.Text = _value?.ToString() ?? $"Null Array";
-        button.Enabled = true;
-    }
+			SetItems(arrayItems, 0);
+		}
+		else
+		{
+			SetItems(_noItems, 0);
+		}
+	}
+
+	protected override void UpdateCurrentOptionText()
+	{
+		var button = (EditorButton?) GetWindowById("Button");
+		if (button == null) return;
+		button.Text = _value?.ToString() ?? "Null Array";
+		button.Enabled = true;
+	}
 }
