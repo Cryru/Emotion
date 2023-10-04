efef85c9a3c5535c27f9e79e1fb512fc9a0a6792
Vlad Abadzhiev <TheCryru@gmail.com>
2023-10-04 09:25:02 +0300

    [3D] Changed default light model sun direction.

--- Emotion/Game/World3D/LightModel.cs
+++ Emotion/Game/World3D/LightModel.cs
@@ -3,7 +3,7 @@
 	public class LightModel
 	{
 		public Color SunColor = Color.White;
-		public Vector3 SunDirection = new Vector3(-0.333093047f, -0.618618965f, 0.7115896f);
+		public Vector3 SunDirection = new Vector3(0.3f, -0.25f, 0.3f);
 		public Color AmbientLightColor = new Color(220, 220, 220);
 	}
 }
