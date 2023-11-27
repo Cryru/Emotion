0374209d4f049fbbd504da7b03e3650e84aacb7f
Vlad Abadzhiev <TheCryru@gmail.com>
2023-11-27 18:13:13 +0200

    [3D] Added ThreeDeeSquareGrid - which is a non-infinite clone of InfiniteGrid

--- Emotion/Game/World3D/Objects/InfiniteGrid.cs
+++ Emotion/Game/World3D/Objects/InfiniteGrid.cs
@@ -10,20 +10,29 @@ using Emotion.Graphics;
 
 namespace Emotion.Game.World3D.Objects;
 
-public sealed class InfiniteGrid : Quad3D
+public class ThreeDeeSquareGrid : Quad3D
 {
-	public float TileSize = 100;
-	public Vector2 Offset;
-	private Vector2 _infiniteGridSize = new Vector2(10_000, 10_000);
-
-	public InfiniteGrid()
+	public float TileSize
 	{
-		Size3D = _infiniteGridSize.ToVec3(1);
+		get => _tileSize;
+		set
+		{
+			_tileSize = value;
+			_tileSize2 = new Vector2(value);
+		}
+	}
+
+	protected float _tileSize = 100;
+	protected Vector2 _tileSize2;
+
+	public Vector2 GridOffset;
+
+	public ThreeDeeSquareGrid()
+	{
 		ObjectFlags |= ObjectFlags.Map3DDontReceiveShadow;
 		ObjectFlags |= ObjectFlags.Map3DDontThrowShadow;
 		ObjectFlags |= ObjectFlags.Map3DDontReceiveAmbient;
 	}
-
 	public override async Task LoadAssetsAsync()
 	{
 		await base.LoadAssetsAsync();
@@ -32,18 +41,36 @@ public sealed class InfiniteGrid : Quad3D
 
 	protected override void RenderInternal(RenderComposer c)
 	{
-		Vector2 cameraPos = c.Camera.Position.ToVec2();
-		Position = cameraPos.ToVec3(Z); // Set position to camera position without the Z
-
-		EntityMetaState!.SetShaderParam("squareSize", new Vector2(TileSize));
-		EntityMetaState.SetShaderParam("cameraPos", (cameraPos + Offset) / _infiniteGridSize);
-		EntityMetaState.SetShaderParam("totalSize", _infiniteGridSize);
+		EntityMetaState!.SetShaderParam("squareSize", _tileSize2);
+		EntityMetaState.SetShaderParam("cameraPos", GridOffset);
+		EntityMetaState.SetShaderParam("totalSize", Size3D.ToVec2());
 
 		base.RenderInternal(c);
 	}
-
 	public override bool IsTransparent()
 	{
 		return true;
+	}
+}
+
+public sealed class InfiniteGrid : ThreeDeeSquareGrid
+{
+	public Vector2 Offset;
+
+	private Vector2 _infiniteGridSize = new Vector2(10_000, 10_000);
+
+	public InfiniteGrid() : base()
+	{
+		Size3D = _infiniteGridSize.ToVec3(1);
+	}
+
+	protected override void RenderInternal(RenderComposer c)
+	{
+		Vector2 cameraPos = c.Camera.Position.ToVec2();
+		Position = cameraPos.ToVec3(Z); // Set position to camera position without the Z
+
+		GridOffset = (Position2 + Offset) / _infiniteGridSize;
+
+		base.RenderInternal(c);
 	}
 }
