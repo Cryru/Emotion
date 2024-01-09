06a8397f2fced0541d6ed8825117d4e3fb7efef9
Vlad Abadzhiev <TheCryru@gmail.com>
2024-01-09 10:26:43 +0200

    [3D] DontReceiveAmbient object rendering flag

--- Emotion/Game/World3D/LightModel.cs
+++ Emotion/Game/World3D/LightModel.cs
@@ -10,5 +10,7 @@
         public float DiffuseStrength = 0.7f;
         public float ShadowOpacity = 0.25f;
         public Vector3 SunDirection = new Vector3(0.3f, -0.25f, 0.3f);
+
+        public static LightModel DefaultLightModel = new LightModel();
     }
 }
