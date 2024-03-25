0c292e6c6f4dc0e63bdc30dd6331da90b6f537f4
Vlad Abadzhiev <TheCryru@gmail.com>
2024-03-25 23:17:36 +0200

    [Utility] Added System.Random extension for NextInclusive

--- Emotion/Utility/Extensions.cs
+++ Emotion/Utility/Extensions.cs
@@ -604,3 +604,14 @@ namespace Emotion.Utility
         }
     }
 }
+
+namespace System
+{
+    public static class RandomExtensions
+    {
+        public static int NextInclusive(this Random rng, int min, int max)
+        {
+            return rng.Next(min, max + 1);
+        }
+    }
+}
