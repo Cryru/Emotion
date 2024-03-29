fca5e0416c4951f3eae935857d393863b08032cb
Vlad Abadzhiev <TheCryru@gmail.com>
2024-03-29 09:54:32 +0200

    [Editor] Fixed GetTypesWhichInherit

--- Emotion/Editor/EditorHelpers/EditorUtility.cs
+++ Emotion/Editor/EditorHelpers/EditorUtility.cs
@@ -83,7 +83,7 @@ public static class EditorUtility
             foreach (Type assemblyType in types)
             {
                 if (!type.IsAssignableFrom(assemblyType)) continue;
-                if (type.IsAbstract) continue;
+                if (assemblyType.IsAbstract) continue;
 
                 bool invalid = assemblyType.GetConstructor(Type.EmptyTypes) == null;
                 if (invalid) continue;
