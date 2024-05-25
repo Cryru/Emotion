5d0005a4a7a0bab724fca3918b297da651e3e280
Vlad Abadzhiev <TheCryru@gmail.com>
2024-05-25 20:40:48 +0300

    [Desktop] Added function for opening urls to DesktopPlatform

--- Emotion/Platform/Implementation/CommonDesktop/DesktopPlatform.cs
+++ Emotion/Platform/Implementation/CommonDesktop/DesktopPlatform.cs
@@ -189,5 +189,35 @@ namespace Emotion.Platform.Implementation.CommonDesktop
                 Engine.Log.Error($"Couldn't display error message box - {message}. {e}", MessageSource.Platform);
             }
         }
+
+        public void OpenUrl(string url)
+        {
+            // https://stackoverflow.com/questions/4580263/how-to-open-in-default-browser-in-c-sharp
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
     }
 }
