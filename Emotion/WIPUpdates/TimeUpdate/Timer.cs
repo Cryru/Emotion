618799f38cb80347ecc261207f54f32796fdd57b
Vlad Abadzhiev <TheCryru@gmail.com>
2024-05-30 20:49:18 +0300

    [WIP] Timers and Interpolation

---
+++ Emotion/WIPUpdates/TimeUpdate/Timer.cs
@@ -0,0 +1,124 @@
+﻿using Emotion.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emotion.WIPUpdates.TimeUpdate;
+
+public enum FactorMethod
+{
+    Linear,
+    Cubic,
+    Exponential,
+    Bounce
+}
+
+public enum FactorType
+{
+    In,
+    Out,
+    InOut
+}
+
+public struct Timer
+{
+    public float Duration;
+    public FactorMethod Method;
+    public FactorType Type;
+
+    public bool Finished;
+    public float TimePassed;
+
+    public Timer(float milliseconds, FactorMethod method = FactorMethod.Linear, FactorType type = FactorType.In)
+    {
+        Duration = milliseconds;
+        Method = method;
+        Type = type;
+    }
+
+    public void Update(float ms)
+    {
+        TimePassed += ms;
+        if (TimePassed >= Duration) Finished = true;
+    }
+
+    public void Reset()
+    {
+        TimePassed = 0;
+        Finished = false;
+    }
+
+    public float GetFactor()
+    {
+        return GetFactorInternal(TimePassed / Duration);
+    }
+
+    public float GetFactorClamped()
+    {
+        return GetFactorInternal(Maths.Clamp01(TimePassed / Duration));
+    }
+
+    private float GetFactorInternal(float t)
+    {
+        switch (Type)
+        {
+            case FactorType.In:
+                t = t;
+                break;
+            case FactorType.Out:
+                t = 1.0f - t;
+                break;
+            case FactorType.InOut:
+                if (t <= 0.5)
+                    t *= 2;
+                else if (t > 0.5)
+                    t = 1.0f - (2f * t - 1.0f);
+                break;
+        }
+
+        switch (Method)
+        {
+            case FactorMethod.Linear:
+                t = t;
+                break;
+            case FactorMethod.Cubic:
+                t = t * t * t;
+                break;
+            case FactorMethod.Exponential:
+                t = 1.0f - MathF.Exp(-10 * t);
+                break;
+            case FactorMethod.Bounce:
+                const float a = 7.5625f;
+                const float b = 1 / 2.75f;
+
+                float res1 = MathF.Min(a * MathF.Pow(t, 2), a * MathF.Pow(t - 1.5f * b, 2) + 0.75f);
+                float res2 = MathF.Min(a * MathF.Pow(t - 2.25f * b, 2) + 0.9375f, a * MathF.Pow(t - 2.625f * b, 2));
+
+                float Bounciness = 1.70158f;
+                t = t * t * ((Bounciness + 1) * t - Bounciness);
+                break;
+        }
+
+        return t;
+    }
+}
+
+public static class Interpolation
+{
+    public static float SmoothLerp(float current, float target, int speed, float delta)
+    {
+        return target + (current - target) * MathF.Exp(-speed * delta);
+    }
+
+    public static Vector2 SmoothLerp(Vector2 current, Vector2 target, int speed, float delta)
+    {
+        return target + (current - target) * MathF.Exp(-speed * delta);
+    }
+
+    public static Vector3 SmoothLerp(Vector3 current, Vector3 target, int speed, float delta)
+    {
+        return target + (current - target) * MathF.Exp(-speed * delta);
+    }
+}
---
+++ Emotion/WIPUpdates/TimeUpdate/TimerDemoScene.cs
@@ -0,0 +1,73 @@
+﻿#region Using
+
+using Emotion.Game.World2D.SceneControl;
+using Emotion.Game.World2D;
+using Emotion.UI;
+using System.Threading.Tasks;
+using Emotion.WIPUpdates.TimeUpdate;
+using System.Reflection.Emit;
+
+#endregion
+
+#nullable enable
+
+namespace Emotion.WIPUpdates.TextUpdate;
+
+public class TimerDemoScene : World2DBaseScene<Map2D>
+{
+    private Timer _linearTimer = new Timer(5000, FactorMethod.Linear, FactorType.InOut);
+    private Timer _cubicTimer = new Timer(5000, FactorMethod.Cubic, FactorType.InOut);
+    private Timer _exponentialTimer = new Timer(5000, FactorMethod.Exponential, FactorType.InOut);
+    private Timer _bounceTimer = new Timer(5000, FactorMethod.Bounce, FactorType.In);
+
+    private UIText _label;
+
+    public override Task LoadAsync()
+    {
+        _editor.EnterEditor();
+
+        UIText label = new()
+        {
+            AnchorAndParentAnchor = UIAnchor.CenterCenter
+        };
+        _editor.UIController!.AddChild(label);
+        _label = label;
+
+        return Task.CompletedTask;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        _label.Text = $"{_linearTimer.GetFactorClamped():0.0}\n{_cubicTimer.GetFactorClamped():0.0}\n{_exponentialTimer.GetFactorClamped():0.0}\n{_bounceTimer.GetFactorClamped():0.0}";
+
+        _linearTimer.Update(Engine.DeltaTime);
+        if (_linearTimer.Finished) _linearTimer.Reset();
+
+        _cubicTimer.Update(Engine.DeltaTime);
+        if (_cubicTimer.Finished) _cubicTimer.Reset();
+
+        _exponentialTimer.Update(Engine.DeltaTime);
+        if (_exponentialTimer.Finished) _exponentialTimer.Reset();
+
+        _bounceTimer.Update(Engine.DeltaTime);
+        if (_bounceTimer.Finished) _bounceTimer.Reset();
+    }
+
+    public override void Draw(RenderComposer composer)
+    {
+        composer.SetUseViewMatrix(false);
+        composer.RenderSprite(Vector3.Zero, composer.CurrentTarget.Size, Color.CornflowerBlue);
+
+        composer.RenderCircle(new Vector3(100, 100, 0) + new Vector3(200, 0, 0) * _linearTimer.GetFactorClamped(), 20, Color.PrettyYellow, true);
+        composer.RenderCircle(new Vector3(100, 150, 0) + new Vector3(200, 0, 0) * _cubicTimer.GetFactorClamped(), 20, Color.PrettyYellow, true);
+        composer.RenderCircle(new Vector3(100, 200, 0) + new Vector3(200, 0, 0) * _exponentialTimer.GetFactorClamped(), 20, Color.PrettyYellow, true);
+        composer.RenderCircle(new Vector3(100, 250, 0) + new Vector3(200, 0, 0) * _bounceTimer.GetFactorClamped(), 20, Color.PrettyYellow, true);
+
+        composer.ClearDepth();
+        composer.SetUseViewMatrix(true);
+
+        base.Draw(composer);
+    }
+}
