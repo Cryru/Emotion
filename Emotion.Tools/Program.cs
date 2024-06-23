dcdc6cbea574ac49c790699edb3d7263beab3c49
Vlad Abadzhiev <TheCryru@gmail.com>
2024-06-23 13:01:38 +0300

    TextDev Setup

--- Emotion.Tools/Program.cs
+++ Emotion.Tools/Program.cs
@@ -1,17 +1,26 @@
 ﻿#region Using
 
+using System;
 using System.Numerics;
+using System.Threading.Tasks;
 using Emotion.Common;
+using Emotion.Common.Threading;
+using Emotion.Game.World2D;
+using Emotion.Game.World2D.SceneControl;
 using Emotion.Graphics;
+using Emotion.Graphics.Objects;
+using Emotion.Graphics.Text.RasterizationNew;
+using Emotion.IO;
 using Emotion.Primitives;
-using Emotion.Scenography;
 using Emotion.UI;
+using OpenGL;
+using static StbTrueTypeSharp.StbTrueType;
 
 #endregion
 
 namespace Emotion.Tools
 {
-    public class Program : IScene
+    public class Program : World2DBaseScene<Map2D>
     {
         private UIController _ui;
 
@@ -34,7 +43,9 @@ namespace Emotion.Tools
             _ui.Update();
         }
 
-        public void Load()
+        private Texture _t;
+
+        public override unsafe Task LoadAsync()
         {
             _ui = new UIController();
             var testBackground = new UISolidColor
@@ -44,16 +55,99 @@ namespace Emotion.Tools
             };
             _ui.AddChild(testBackground);
             ToolsManager.AddToolBoxWindow(_ui);
+
+
+            OtherAsset ass = Engine.AssetLoader.Get<OtherAsset>(FontAsset.DefaultBuiltInFontName, false);
+            stbtt_fontinfo fontInfo = new stbtt_fontinfo();
+            var cnt = ass.Content.Span;
+            fixed (byte* ptr = &cnt[0])
+            {
+                stbtt_InitFont(fontInfo, ptr, 0);
+            }
+
+            var c = 'T';
+
+            int ascent, descent, lineGap;
+            stbtt_GetFontVMetrics(fontInfo, &ascent, &descent, &lineGap);
+
+            float xpos = 0;
+            float m_scale = 28 / (float) ascent;
+
+            int advance;
+            int lsb;
+            stbtt_GetCodepointHMetrics(fontInfo, c, &advance, &lsb);
+
+            int w, h, x0, y0, x1, y1;
+            float x_shift = xpos - MathF.Floor(xpos);
+            stbtt_GetCodepointBitmapBoxSubpixel(fontInfo, c, m_scale, m_scale, x_shift, 0, &x0, &y0, &x1, &y1);
+
+            w = x1 - x0;
+            h = y1 - y0;
+
+            if (xpos + lsb * m_scale < 0) xpos -= lsb * m_scale;
+
+            var glyph_pitch = w;
+            var glyphIndex = stbtt_FindGlyphIndex(fontInfo, c);
+            byte[] pbm = new byte[w * h];
+
+            fixed (byte* pPbm = &pbm[0])
+            {
+                stbtt_MakeGlyphBitmapSubpixel(fontInfo, pPbm, w, h, glyph_pitch, m_scale, m_scale, x_shift, 0, glyphIndex);
+            }
+
+            SoftwareGlyphRasterizer.RenderGlyph(fontInfo, pbm, w, h, glyph_pitch, m_scale, m_scale, x_shift, 0, glyphIndex);
+
+            int startx = 0;
+            int starty = 0;
+
+            // clamp horizontally
+            //int left = (int) MathF.Floor(xpos + lsb * m_scale);
+            //if (left < 0) {
+            //	startx -= left;
+            //	left = 0;
+            //}
+            //if (left + w > destWidth) {
+            //	w = destWidth - left;
+            //}
+
+            //// clamp vertically
+            //int top = m_baseline + y0;
+            //if (top < 0) {
+            //	starty -= top;
+            //	top = 0;
+            //}
+            //if (top + h >(int)destHeight) {
+            //	h = destHeight - top;
+            //}
+
+            xpos += m_scale * advance;
+
+            GLThread.ExecuteGLThread(() =>
+            {
+                _t = new Texture();
+                _t.Upload(new Vector2(w, h), pbm, PixelFormat.Red, InternalFormat.Red, PixelType.UnsignedByte);
+            });
+
+            _font = fontInfo;
+
+            return Task.CompletedTask;
         }
 
-        public void Unload()
+        private stbtt_fontinfo _font;
+
+        public override void Unload()
         {
         }
 
-        public void Draw(RenderComposer composer)
+        public override void Draw(RenderComposer composer)
         {
             composer.SetUseViewMatrix(false);
             _ui.Render(composer);
+
+            composer.RenderSprite(new Vector3(100, 100, 0), _t.Size, Color.White, _t);
+
+            //var font = Engine.AssetLoader.Get<FontAsset>("SourceCodePro-Regular.ttf");
+            //composer.RenderString(new Vector3(100, 100, 0), Color.White, "T", font.GetAtlas(18));
         }
     }
 }
--- Emotion/WIPUpdates/TextUpdate/TextUpdateDemoScene.cs
+++ Emotion/WIPUpdates/TextUpdate/TextUpdateDemoScene.cs
@@ -15,6 +15,7 @@ public class TextUpdateDemoScene : World2DBaseScene<Map2D>
 {
     public override Task LoadAsync()
     {
+        TextLayoutEngine.TestNewRenderer = true;
         _editor.EnterEditor();
 
         //UIRichText testText = new UIRichText();
@@ -37,7 +38,7 @@ public class TextUpdateDemoScene : World2DBaseScene<Map2D>
         _editor.UIController!.AddChild(container);
 
         UITextInput2 textInput = new UITextInput2();
-        textInput.Text = "The quick <color #c26e2f>brown</> fox <color 255 125 255>jumped</> over the lazy dog.";
+        textInput.Text = "The quick brown fox jumped";
         textInput.MultiLine = true;
         textInput.MaxSizeX = 100;
         textInput.MaxSizeY = 150;
