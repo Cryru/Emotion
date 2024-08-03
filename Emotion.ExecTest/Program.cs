a1cd5d225e0907b972e365f058ec721fdf5b70c1
Vlad Abadzhiev <TheCryru@gmail.com>
2024-08-03 19:31:49 +0300

    [One] Demo

--- Emotion.ExecTest/Program.cs
+++ Emotion.ExecTest/Program.cs
@@ -13,10 +13,15 @@ using Emotion.Game.World3D;
 using Emotion.Game.World3D.SceneControl;
 using Emotion.Graphics;
 using Emotion.Graphics.Camera;
+using Emotion.IO;
+using Emotion.Platform.Input;
 using Emotion.Primitives;
 using Emotion.Scenography;
 using Emotion.Testing;
 using Emotion.Utility;
+using Emotion.WIPUpdates.One;
+using Emotion.WIPUpdates.One.Work;
+using WinApi.User32;
 
 #endregion
 
@@ -32,7 +37,8 @@ public class Program
             return;
         }
 
-        Engine.Start(new Configurator {
+        Engine.Start(new Configurator
+        {
             DebugMode = true,
             HostTitle = "Example"
         }, EntryPointAsync);
@@ -51,7 +57,45 @@ public class Program
     private static IEnumerator EntryPointAsync()
     {
         yield return Engine.SceneManager.SetScene(new TestScene());
-        
+
+    }
+}
+
+public class TestObject : MapObject
+{
+    private Vector2 _inputDirection;
+
+    public TestObject()
+    {
+        Size = new Vector2(20);
+
+        Engine.Host.OnKey.AddListener(KeyInput, KeyListenerType.Game);
+    }
+
+    protected bool KeyInput(Key key, Platform.Input.KeyState status)
+    {
+        Vector2 keyAxisPart = Engine.Host.GetKeyAxisPart(key, Key.AxisWASD);
+        if (keyAxisPart != Vector2.Zero)
+        {
+            if (status == Platform.Input.KeyState.Down)
+                _inputDirection += keyAxisPart;
+            else if (status == Platform.Input.KeyState.Up)
+                _inputDirection -= keyAxisPart;
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public override void Update(float dt)
+    {
+        Position2 += _inputDirection * 0.1f * dt;
+    }
+
+    public override void Render(RenderComposer c)
+    {
+        c.RenderSprite(Position, Size, Color.Red);
     }
 }
 
@@ -59,21 +103,30 @@ public class TestScene : Scene
 {
     protected override IEnumerator LoadSceneRoutineAsync()
     {
+        // Fixes deadlock
+        Engine.AssetLoader.Get<ShaderAsset>("FontShaders/SDF.xml");
+
+        Map = new GameMap();
+        //Map.AddAndInitObject(new MapObjectSprite()
+        //{
+        //    EntityFile = "Test/Character.em2"
+        //});
+
+        Map.AddAndInitObject(new TestObject());
+
         //throw new System.Exception("haa");
         yield break;
     }
 
-    protected override void RenderScene(RenderComposer c)
+    public override void RenderScene(RenderComposer c)
     {
         c.SetUseViewMatrix(false);
         c.RenderSprite(Vector3.Zero, c.CurrentTarget.Size, Color.PrettyGreen);
         c.ClearDepth();
         c.SetUseViewMatrix(true);
-    }
 
-    protected override void UpdateScene(float dt)
-    {
-        
+        c.RenderCircle(Vector3.Zero, 20, Color.White, true);
+        base.RenderScene(c);
     }
 }
 
