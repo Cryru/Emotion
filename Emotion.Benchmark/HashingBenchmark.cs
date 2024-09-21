a266e47062065db56fdd05a31ef1e0af953c8bad
Vlad Abadzhiev <TheCryru@gmail.com>
2024-09-21 15:25:17 +0300

    Added xxhash

---
+++ Emotion.Benchmark/HashingBenchmark.cs
@@ -0,0 +1,43 @@
+﻿#region Using
+
+using BenchmarkDotNet.Attributes;
+using Emotion.Common;
+using Emotion.IO;
+using Emotion.Standard.Image.ImgBin;
+using Emotion.Standard.Image.PNG;
+
+#endregion
+
+namespace Emotion.Benchmark
+{
+    [MemoryDiagnoser]
+    public class HashingBenchmark
+    {
+        private ReadOnlyMemory<byte> _fileBytes;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            Configurator config = new Configurator();
+            config.HiddenWindow = true;
+            Engine.Setup(config);
+
+            var asset = Engine.AssetLoader.Get<OtherAsset>("logoAlpha.png");
+            _fileBytes = asset.Content;
+        }
+
+        [Benchmark]
+        public void XXHash()
+        {
+            _fileBytes.Span.GetStableHashCode();
+            "random string".GetStableHashCode();
+        }
+        
+        [Benchmark]
+        public void Md5()
+        {
+            _fileBytes.Span.__NoDepend_GetStableHashCode();
+            "random string".__NoDepend_GetStableHashCode();
+        }
+    }
+}
