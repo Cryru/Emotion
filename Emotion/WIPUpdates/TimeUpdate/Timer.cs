9af4dd86bf751175e08db1f77aa81b0215d53198
Vlad Abadzhiev <TheCryru@gmail.com>
2024-12-12 23:11:35 +0200

    [Time] Added SmoothLerpAngle

--- Emotion/WIPUpdates/TimeUpdate/Timer.cs
+++ Emotion/WIPUpdates/TimeUpdate/Timer.cs
@@ -115,6 +115,15 @@ public static class Interpolation
         return target + (current - target) * MathF.Exp(-speed * delta);
     }
 
+    public static float SmoothLerpAngle(float current, float target, int speed, float delta)
+    {
+        float num = Maths.Repeat(current - target, 360f);
+        if (num > 180f)
+            num -= 360f;
+
+        return target + num * MathF.Exp(-speed * delta);
+    }
+
     public static Vector2 SmoothLerp(Vector2 current, Vector2 target, int speed, float deltaMs)
     {
         float delta = deltaMs / 1000f;
