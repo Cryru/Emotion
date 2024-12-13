0c01dce15f62a2447c64dd5ca63ae8701a0f4bc8
Vlad Abadzhiev <TheCryru@gmail.com>
2024-12-13 08:24:55 +0200

    [Tests] More tests updated

---
+++ Tests/EngineTests/ShaderTests.cs
@@ -0,0 +1,148 @@
+﻿#region Using
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Emotion.Common;
+using Emotion.IO;
+using Emotion.Primitives;
+using Emotion.Testing;
+
+#endregion
+
+namespace Tests.EngineTests;
+
+public class ShaderTests : ProxyRenderTestingScene
+{
+    /// <summary>
+    /// Test whether loading of shaders and drawing with them works.
+    /// Also tests loading of shaders with vert and without frag.
+    /// </summary>
+    [Test]
+    public IEnumerator ShaderTest()
+    {
+        var shaders = new List<ShaderAsset>();
+
+        // Which phase of the test is happening. Done to split the draw function.
+        var shaderTest = 0;
+
+        shaders.Add(Engine.AssetLoader.Get<ShaderAsset>("Shaders/TestShader.xml"));
+        shaders.Add(Engine.AssetLoader.Get<ShaderAsset>("Shaders/TestShaderFragOnly.xml"));
+        shaders.Add(Engine.AssetLoader.Get<ShaderAsset>("Shaders/TestShaderVertOnly.xml"));
+
+        // Ensure all shaders are loaded.
+        foreach (ShaderAsset s in shaders)
+        {
+            Assert.True(s.Shader != null);
+            Assert.False(s.IsFallback);
+        }
+
+        ToRender = (composer) =>
+        {
+            composer.SetUseViewMatrix(false);
+
+            // Set shader.
+            if (shaderTest != 0)
+                composer.SetShader(shaders[shaderTest - 1].Shader);
+
+            // Render a blank square.
+            composer.RenderSprite(new Vector3(10, 10, 0), new Vector2(10, 10), Color.White);
+
+            // Reset to default shader.
+            composer.SetShader();
+        };
+
+        // Change to phase 1. This is drawing with the test shader.
+        shaderTest = 0;
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        shaderTest = 1;
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        // Change to phase 2. This is drawing with the vert missing shader.
+        shaderTest = 2;
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        // Change to phase 3. This is drawing with the frag missing shader.
+        shaderTest = 3;
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        // Cleanup
+        foreach (ShaderAsset s in shaders)
+        {
+            Engine.AssetLoader.Destroy(s.Name);
+        }
+
+        // Ensure the shaders are unloaded.
+        Assert.Equal(shaders.Count, shaders.Select(x => x.Name).Except(Engine.AssetLoader.LoadedAssets.Select(x => x.Name)).Count());
+    }
+
+    /// <summary>
+    /// Test whether loading of broken shaders behaves as expected.
+    /// </summary>
+    [Test]
+    public IEnumerator ShaderBrokenLoad()
+    {
+        var shader = Engine.AssetLoader.Get<ShaderAsset>("Shaders/BrokenShader.xml");
+
+        ToRender = (composer) =>
+        {
+            composer.SetUseViewMatrix(false);
+
+            // Set shader.
+            composer.SetShader(shader.Shader);
+
+            // Render a blank square.
+            composer.RenderSprite(new Vector3(10, 10, 0), new Vector2(10, 10), Color.White);
+
+            // Reset to default shader.
+            composer.SetShader();
+        };
+
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        // Even though the shader is broken, and it doesn't specify a fallback, it should load the compat shader.
+        Assert.True(shader.Shader != null);
+        Assert.True(shader.IsFallback);
+
+        Engine.AssetLoader.Destroy(shader.Name);
+    }
+
+    /// <summary>
+    /// Test whether shader fallback works.
+    /// </summary>
+    [Test]
+    public IEnumerator ShaderFallback()
+    {
+        var shader = Engine.AssetLoader.Get<ShaderAsset>("Shaders/BrokenShaderWithFallback.xml");
+
+        ToRender = (composer) =>
+        {
+            composer.SetUseViewMatrix(false);
+
+            // Set shader.
+            composer.SetShader(shader.Shader);
+
+            // Render a blank square.
+            composer.RenderSprite(new Vector3(10, 10, 0), new Vector2(10, 10), Color.White);
+
+            // Reset to default shader.
+            composer.SetShader();
+        };
+
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        // The shader should've loaded its fallback.
+        Assert.True(shader.Shader != null);
+        Assert.True(shader.IsFallback);
+
+        Engine.AssetLoader.Destroy(shader.Name);
+    }
+}
---
+++ Tests/EngineTests/StandardImageTests.cs
@@ -0,0 +1,170 @@
+﻿#region Using
+
+using System.Collections;
+using System.IO;
+using System.Numerics;
+using Emotion.Graphics.Objects;
+using Emotion.Primitives;
+using Emotion.Standard.Image.BMP;
+using Emotion.Standard.Image.PNG;
+using Emotion.Testing;
+using OpenGL;
+
+#endregion
+
+namespace Tests.EngineTests;
+
+public class StandardImageTests : ProxyRenderTestingScene
+{
+    [Test]
+    public IEnumerator DecodeBmp16()
+    {
+        string bmp16 = Path.Join("Assets", "Images", "16colorbmp.bmp");
+        byte[] bytes = File.ReadAllBytes(bmp16);
+        Assert.True(BmpFormat.IsBmp(bytes));
+
+        byte[] decodedPixelData = BmpFormat.Decode(bytes, out BmpFileHeader fileHeader);
+        Assert.True(decodedPixelData != null);
+
+        Texture texture = null;
+
+        ToRender = (composer) =>
+        {
+            texture = new Texture(
+                new Vector2(fileHeader.Width, fileHeader.Height),
+                decodedPixelData,
+                PixelFormat.Bgra
+            )
+            { FlipY = true };
+
+            composer.SetUseViewMatrix(false);
+            composer.RenderSprite(Vector3.Zero, new Vector2(fileHeader.Width, fileHeader.Height), Color.White, texture);
+        };
+
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        texture.Dispose();
+    }
+
+    [Test]
+    public IEnumerator DecodeBmp24()
+    {
+        string bmp24 = Path.Join("Assets", "Images", "24bitbmp.bmp");
+        byte[] bytes = File.ReadAllBytes(bmp24);
+        Assert.True(BmpFormat.IsBmp(bytes));
+
+        byte[] decodedPixelData = BmpFormat.Decode(bytes, out BmpFileHeader fileHeader);
+        Assert.True(decodedPixelData != null);
+
+        Texture texture = null;
+
+        ToRender = (composer) =>
+        {
+            texture = new Texture(
+                new Vector2(fileHeader.Width, fileHeader.Height),
+                decodedPixelData,
+                PixelFormat.Bgra
+            )
+            { FlipY = true };
+
+            composer.SetUseViewMatrix(false);
+            composer.RenderSprite(Vector3.Zero, new Vector2(fileHeader.Width, fileHeader.Height), Color.White, texture);
+        };
+
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        texture.Dispose();
+    }
+
+    [Test]
+    public IEnumerator DecodeBmp256()
+    {
+        string bmp256 = Path.Join("Assets", "Images", "256colorbmp.bmp");
+        byte[] bytes = File.ReadAllBytes(bmp256);
+        Assert.True(BmpFormat.IsBmp(bytes));
+
+        byte[] decodedPixelData = BmpFormat.Decode(bytes, out BmpFileHeader fileHeader);
+        Assert.True(decodedPixelData != null);
+
+        Texture texture = null;
+
+        ToRender = (composer) =>
+        {
+            texture = new Texture(
+                new Vector2(fileHeader.Width, fileHeader.Height),
+                decodedPixelData,
+                PixelFormat.Bgra
+            )
+            { FlipY = true };
+
+            composer.SetUseViewMatrix(false);
+            composer.RenderSprite(Vector3.Zero, new Vector2(fileHeader.Width, fileHeader.Height), Color.White, texture);
+        };
+
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        texture.Dispose();
+    }
+
+    [Test]
+    public IEnumerator DecodePng()
+    {
+        string png = Path.Join("Assets", "Images", "standardPng.png");
+        byte[] bytes = File.ReadAllBytes(png);
+        Assert.True(PngFormat.IsPng(bytes));
+
+        byte[] decodedPixelData = PngFormat.Decode(bytes, out PngFileHeader fileHeader);
+        Assert.True(decodedPixelData != null);
+
+        Texture texture = null;
+
+        ToRender = (composer) =>
+        {
+            texture = new Texture(
+                fileHeader.Size,
+                decodedPixelData,
+                fileHeader.PixelFormat);
+
+            composer.SetUseViewMatrix(false);
+            composer.RenderSprite(Vector3.Zero, fileHeader.Size, Color.White, texture);
+        };
+
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        texture.Dispose();
+    }
+
+    [Test]
+    public IEnumerator DecodePngInterlaced()
+    {
+        string png = Path.Join("Assets", "Images", "spritesheetAnimation.png");
+        byte[] bytes = File.ReadAllBytes(png);
+        Assert.True(PngFormat.IsPng(bytes));
+
+        byte[] decodedPixelData = PngFormat.Decode(bytes, out PngFileHeader fileHeader);
+        Assert.True(decodedPixelData != null);
+        Assert.True(fileHeader.InterlaceMethod == 1);
+
+        Texture texture = null;
+
+        ToRender = (composer) =>
+        {
+            texture = new Texture(
+                fileHeader.Size,
+                decodedPixelData,
+                fileHeader.PixelFormat);
+
+            composer.SetUseViewMatrix(false);
+            composer.RenderSprite(Vector3.Zero, fileHeader.Size, Color.White, texture);
+        };
+
+        yield return new TestWaiterRunLoops(1);
+        VerifyScreenshot();
+
+        texture.Dispose();
+    }
+}
