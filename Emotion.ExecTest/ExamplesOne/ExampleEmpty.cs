dcbe4ef7e0cc8864a188bcc4e9ff303b167bc370
Vlad Abadzhiev <TheCryru@gmail.com>
2025-01-09 14:25:46 +0200

    Added empty example scene in ExecTest

---
+++ Emotion.ExecTest/ExamplesOne/ExampleEmpty.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+using Emotion.Scenography;
+using Emotion.WIPUpdates.One;
+using System.Collections;
+
+namespace Emotion.ExecTest.ExamplesOne;
+
+public class ExampleEmpty : SceneWithMap
+{
+    protected override IEnumerator InternalLoadSceneRoutineAsync()
+    {
+        Map = new GameMap();
+
+        yield break;
+    }
+}
--- Emotion.ExecTest/Program.cs
+++ Emotion.ExecTest/Program.cs
@@ -47,6 +47,6 @@ public class Program
     private static IEnumerator EntryPointAsync()
     {
         //yield return Engine.SceneManager.SetScene(new TestScene());
-        yield return Engine.SceneManager.SetScene(new Example3D());
+        yield return Engine.SceneManager.SetScene(new ExampleEmpty());
     }
 }
