551eb9752a416f88af60985f53a25efe52ce6aff
HHristova <86851411+GrblackStar@users.noreply.github.com>
2025-01-19 17:51:29 +0200

    Particle System Initial Commit

---
+++ Emotion/Game/Particles/Particle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp.Formats.Png;
+
+namespace Emotion.Game.Particles
+{
+    public class Particle
+    {
+        public Vector3 Position;
+        public float AliveTime;
+        public Vector3 TargetDirection;
+    }
+}
