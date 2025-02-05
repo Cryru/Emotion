e7723f4ac8753ddf1a6f7c625e5913bb6ae85faf
Vlad Abadzhiev <TheCryru@gmail.com>
2025-02-05 17:40:30 +0200

    [Editor] Checkbox button

---
+++ Emotion/WIPUpdates/One/EditorUI/Components/EditorCheckboxButton.cs
@@ -0,0 +1,49 @@
+﻿#nullable enable
+
+using Emotion.Common.Serialization;
+using Emotion.UI;
+
+namespace Emotion.WIPUpdates.One.EditorUI.Components;
+
+public class EditorCheckboxButton : SquareEditorButton
+{
+    [DontSerialize]
+    public Action<bool>? OnValueChanged;
+
+    public bool Value
+    {
+        get => _value;
+        set
+        {
+            if (_value == value) return;
+            _value = value;
+            _checkIcon.Visible = value;
+        }
+    }
+
+    private bool _value;
+
+    private UITexture _checkIcon;
+
+    public EditorCheckboxButton() : base()
+    {
+        UITexture checkIcon = new UITexture()
+        {
+            TextureFile = "Editor/Checkmark.png",
+            ImageScale = new Vector2(0.75f),
+            Smooth = true,
+            AnchorAndParentAnchor = UIAnchor.CenterCenter,
+            IgnoreParentColor = true
+        };
+        AddChild(checkIcon);
+        _checkIcon = checkIcon;
+    }
+
+    protected override void OnClicked()
+    {
+        base.OnClicked();
+
+        Value = !Value;
+        OnValueChanged?.Invoke(Value);
+    }
+}
