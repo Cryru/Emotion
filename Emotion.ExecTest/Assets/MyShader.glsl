b07524dcaba703bb07c0dbccf7770e43ff875b79
HHristova <86851411+GrblackStar@users.noreply.github.com>
2025-02-09 16:34:19 +0200

    New shader format functionality - version, defines and main

---
+++ Emotion.ExecTest/Assets/MyShader.glsl
@@ -0,0 +1,44 @@
+﻿vec2 Something()
+{
+    return vec2(1.0);
+}
+//
+vec2 SomethingTwo()
+{
+#if VERT_SHADER
+    return vec2(1.0);
+#endif
+#if FRAG_SHADER
+    return vec2(0.0);
+#endif
+}
+
+#if VERT_SHADER
+uniform mat4 projectionMatrix; 
+uniform mat4 viewMatrix; 
+uniform mat4 modelMatrix; 
+ 
+layout(location = 0)in vec3 vertPos; 
+layout(location = 1)in vec2 uv; 
+layout(location = 2)in vec4 color; 
+ 
+// Goes to the frag shader.  
+out vec2 UV; 
+out vec4 vertColor; 
+
+void VertexShaderMain() { 
+    // Pass to frag.
+    UV = uv;
+    vertColor = color;
+    
+    // Multiply by projection.
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vertPos, 1.0);
+}
+#endif
+
+#if FRAG_SHADER
+vec4 FragmentShaderMain()
+{
+    return vec4(1.0, 0.0, 0.0, 1.0);
+}
+#endif
