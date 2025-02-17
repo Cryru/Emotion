bbe94dc491dfd5d32dbbe39f8e3c86413ed1f2e5
Vlad Abadzhiev <TheCryru@gmail.com>
2025-02-18 00:05:09 +0200

    [NewShaders] ShaderTestScene Light Refactor

--- Emotion.ExecTest/ExamplesOne/ShaderTestScene.cs
+++ Emotion.ExecTest/ExamplesOne/ShaderTestScene.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Emotion.Common;
 using Emotion.Common.Threading;
 using Emotion.Graphics;
+using Emotion.Graphics.Shader;
 using Emotion.Graphics.Shading;
 using Emotion.IO;
 using Emotion.Primitives;
@@ -15,38 +16,6 @@ using Emotion.Scenography;
 using OpenGL;
 
 namespace Emotion.ExecTest.ExamplesOne;
-
-public class NewShaderAsset : TextAsset
-{
-    public ShaderProgram CompiledShader;
-
-    protected override void CreateInternal(ReadOnlyMemory<byte> data)
-    {
-        base.CreateInternal(data);
-        
-        // Content <- The content of our file :)
-        bool es = Gl.CurrentShadingVersion.GLES;
-        string versionString = $"#version {Gl.CurrentShadingVersion.VersionId}{(es ? " es" : "")}\n";
-
-        StringBuilder vertexShader = new StringBuilder();
-        vertexShader.Append(versionString);
-        vertexShader.Append("\n#define VERT_SHADER 1\n");
-        vertexShader.Append(Content);
-        vertexShader.Append("\n");
-        vertexShader.Append("void main()\n{\nVertexShaderMain();\n}");
-
-        StringBuilder fragmentShader = new StringBuilder();
-        fragmentShader.Append(versionString);
-        fragmentShader.Append("\n#define FRAG_SHADER 1\n");
-        fragmentShader.Append(Content);
-        fragmentShader.Append("\n");
-        fragmentShader.Append("\nout vec4 fragColor;\n");
-        fragmentShader.Append("void main()\n{\nfragColor = FragmentShaderMain();\n}");
-
-        CompiledShader?.Dispose();
-        CompiledShader = GLThread.ExecuteGLThread(ShaderFactory.CreateShaderRaw, vertexShader.ToString(), fragmentShader.ToString());
-    }
-}
 
 public class ShaderTestScene : SceneWithMap
 {
