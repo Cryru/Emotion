fffb48f2b770ebd1310f1c3fd4f22d9b9a83accb
Vlad Abadzhiev <TheCryru@gmail.com>
2025-02-26 13:13:32 +0200

    [ONE] Added a demo WoW camera and movement controller

---
+++ Emotion/Game/PremadeControllers/WorldOfWarcraft/WoWCamera.cs
@@ -0,0 +1,95 @@
+﻿#nullable enable
+
+using Emotion;
+using Emotion.Graphics.Camera;
+using Emotion.Platform.Input;
+using Emotion.Utility;
+using Emotion.WIPUpdates.One.Work;
+
+namespace Emotion.Game.PremadeControllers.WorldOfWarcraft;
+
+public class WoWCamera : Camera3D
+{
+    public float Distance = 2000;
+
+    public MapObject? Target;
+    public Vector3 TargetOffset;
+
+    private bool _mouseHeldLeft = false;
+    private bool _mouseHeldRight = false;
+
+    public WoWCamera(Vector3 position, float zoom = 1, KeyListenerType inputPriority = KeyListenerType.Game) : base(position, zoom, inputPriority)
+    {
+        _invertXMouseMovement = true;
+        _cameraMovementSpeed = 0.15f;
+    }
+
+    public void SetTarget(MapObject? target, Vector3 targetOffset)
+    {
+        Target = target;
+        TargetOffset = targetOffset;
+        if (target != null)
+        {
+            LookAtPoint(target.Position);
+            _yawRollPitch = new Vector3(180, 0, 45);
+        }
+    }
+
+    protected override bool CameraKeyHandler(Key key, KeyState status)
+    {
+        if (key == Key.MouseKeyRight || key == Key.MouseKeyLeft)
+        {
+            if (key == Key.MouseKeyRight)
+            {
+                if (status == KeyState.Down)
+                    _mouseHeldRight = true;
+
+                if (status == KeyState.Up)
+                    _mouseHeldRight = false;
+            }
+
+            if (key == Key.MouseKeyLeft)
+            {
+                if (status == KeyState.Down)
+                    _mouseHeldLeft = true;
+
+                if (status == KeyState.Up)
+                    _mouseHeldLeft = false;
+            }
+
+            bool anyHeld = _mouseHeldLeft || _mouseHeldRight;
+            if (anyHeld != _mouseKeyHeld)
+            {
+                if (anyHeld)
+                    _lastMousePos = Engine.Host.MousePosition;
+                _mouseKeyHeld = anyHeld;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Target != null)
+        {
+            var direction = new Vector3
+            {
+                X = MathF.Cos(Maths.DegreesToRadians(_yawRollPitch.Z)) * MathF.Cos(Maths.DegreesToRadians(_yawRollPitch.X)),
+                Y = MathF.Cos(Maths.DegreesToRadians(_yawRollPitch.Z)) * MathF.Sin(Maths.DegreesToRadians(_yawRollPitch.X)),
+                Z = MathF.Sin(Maths.DegreesToRadians(_yawRollPitch.Z))
+            };
+            direction = Vector3.Normalize(direction);
+
+            Vector3 focusPosition = Target.Position + TargetOffset;
+
+            Position = focusPosition + direction * Distance;
+            _lookAt = Vector3.Normalize(focusPosition - Position);
+            RecreateViewMatrix();
+        }
+    }
+}
---
+++ Emotion/Game/PremadeControllers/WorldOfWarcraft/WoWMovementController.cs
@@ -0,0 +1,128 @@
+﻿#nullable enable
+
+using Emotion.Graphics.Camera;
+using Emotion.Platform.Input;
+using Emotion.WIPUpdates.One;
+using Emotion.WIPUpdates.One.Work;
+
+namespace Emotion.Game.PremadeControllers.WorldOfWarcraft;
+
+public class WoWMovementController
+{
+    public float WalkingSpeed = 0.5f; // Per millisecond
+
+    private MapObject? _character;
+    private string? _idleAnim;
+    private string? _walkAnim;
+    private string? _strafeLeftAnim;
+    private string? _strafeRightAnim;
+    private string? _walkBackAnim;
+
+    private Vector2 _input;
+    private bool _leftClickHeld;
+    private bool _rightClickHeld;
+
+    private WoWCamera _camera = new WoWCamera(Vector3.Zero);
+
+    public void Attach()
+    {
+        Engine.Host.OnKey.AddListener(KeyHandler, KeyListenerType.Game);
+        Engine.Renderer.Camera = _camera;
+    }
+
+    public void Dettach()
+    {
+        Engine.Host.OnKey.RemoveListener(KeyHandler);
+    }
+
+    public void SetCharacter(MapObject obj, string idleAnim, string walkAnim, string strafeLeftAnim, string strafeRightAnim, string walkBackAnim)
+    {
+        _camera.SetTarget(obj, new Vector3(0, 0, -50));
+
+        _character = obj;
+        _idleAnim = idleAnim;
+        _walkAnim = walkAnim;
+        _strafeLeftAnim = strafeLeftAnim;
+        _strafeRightAnim = strafeRightAnim;
+        _walkBackAnim = walkBackAnim;
+    }
+
+    public void Update(float dt)
+    {
+        if (_character == null) return;
+
+        GameMap? map = _character.Map;
+        if (map == null) return;
+
+        Vector2 wasd = _input;
+
+        if (_rightClickHeld && _leftClickHeld)
+            wasd = new Vector2(0, -1);
+
+        if (wasd != Vector2.Zero)
+        {
+            wasd = wasd.Perpendicular();
+            wasd = -wasd;
+            wasd = _character.RotateVectorToObjectFacing(wasd);
+        }
+
+        Vector2 forward = new Vector2(1, 0);
+        forward = _character.RotateVectorToObjectFacing(forward);
+
+        bool moved = wasd != Vector2.Zero;
+        bool walkingBack = Vector2.Dot(wasd, forward) < 0;
+
+        float movementSpeed = WalkingSpeed;
+        if (walkingBack) movementSpeed /= 2f;
+
+        _character.Position += (wasd * movementSpeed * dt).ToVec3();
+
+        if (moved && map.TerrainGrid != null)
+        {
+            float height = map.TerrainGrid.GetHeightAt(_character.Position2D);
+            _character.Z = height;
+        }
+
+        if (_rightClickHeld && _character is MapObjectMesh meshObj)
+        {
+            CameraBase camera = Engine.Renderer.Camera;
+            Vector3 lookAtPos = camera.LookAt;
+            meshObj.RotateToFacePoint(_character.Position + lookAtPos);
+        }
+
+        if (_character is MapObjectMesh animatedObj)
+        {
+            string? animToSet = moved ? _walkAnim : _idleAnim;
+
+            //if (wasd.X < 0) animToSet = _strafeLeftAnim;
+            //if (wasd.X > 0) animToSet = _strafeRightAnim;
+            if (walkingBack) animToSet = _walkBackAnim;
+
+            if (animToSet != null && animToSet != animatedObj.GetCurrentAnimation())
+                animatedObj.SetAnimation(animToSet);
+        }
+    }
+
+    private bool KeyHandler(Key key, KeyState status)
+    {
+        Vector2 axis = Engine.Host.GetKeyAxisPart(key, Key.AxisWASD);
+        if (axis != Vector2.Zero)
+        {
+            if (status == KeyState.Up) axis = -axis;
+            _input += axis;
+            return false;
+        }
+
+        if (key == Key.MouseKeyRight)
+        {
+            _rightClickHeld = status == KeyState.Down;
+        }
+
+        if (key == Key.MouseKeyLeft)
+        {
+            _leftClickHeld = status == KeyState.Down;
+        }
+
+        return true;
+    }
+}
