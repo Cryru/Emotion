a0789841516568ecab394302b38fdd6d9b33c68d
Vlad Abadzhiev <TheCryru@gmail.com>
2025-03-12 14:51:53 +0200

    [JSON] Deserialization state machine experiment

--- Emotion/Serialization/Json/JsonSerialization.cs
+++ Emotion/Serialization/Json/JsonSerialization.cs
@@ -6,6 +6,7 @@ using Emotion.Standard.Reflector.Handlers;
 using Emotion.Standard.Reflector;
 using System.Text;
 using Emotion.Standard.OptimizedStringReadWrite;
+using Emotion.Utility;
 
 namespace Emotion.Serialization.JSON;
 
@@ -31,13 +32,411 @@ public static class JSONSerialization
     public static T? From<T>(ReadOnlySpan<char> jsonDataUtf16)
     {
         var reader = new ValueStringReader(jsonDataUtf16);
-        return From<T>(ref reader);
+        return FromStateMachine<T>(ref reader);
     }
 
     public static T? From<T>(ReadOnlySpan<byte> jsonDataUtf8)
     {
         var reader = new ValueStringReader(jsonDataUtf8);
-        return From<T>(ref reader);
+        return FromStateMachine<T>(ref reader);
+    }
+
+    private enum JSONReadState
+    {
+        None,
+        DetermineNextRead,
+        PostReadValue,
+
+        StartObject,
+        ReadObjectKeyValue,
+
+        StartArray,
+
+        ReadString,
+        ReadNumber,
+        ReadBoolean
+    }
+
+    private class JSONStackEntry
+    {
+        // It is possible for both the member and the object to not be defined in the
+        // C# structure while being defined inthe JSON structure.
+        // In this case we want to read past them still - so handle cases in which reading is done
+        // implicitly (ie. by reflector methods).
+        public object? CurrentObject;
+        public object? ParentObject;
+        public bool ParentIsList;
+        public ComplexTypeHandlerMember? ObjectMember;
+        public IGenericReflectorTypeHandler? TypeHandler;
+
+        public void Reset()
+        {
+            CurrentObject = null;
+            ParentObject = null;
+            ParentIsList = false;
+            ObjectMember = null;
+            TypeHandler = null;
+        }
+    }
+
+    private static ObjectPool<JSONStackEntry> _stackEntryPool = new ObjectPool<JSONStackEntry>((r) => r.Reset(), 10);
+
+    public unsafe static T? FromStateMachine<T>(ref ValueStringReader reader)
+    {
+        ReflectorTypeHandlerBase<T>? typeHandler = ReflectorEngine.GetTypeHandler<T>();
+        if (typeHandler == null) return default;
+
+        ComplexTypeHandler<T>? complexHandler = typeHandler as ComplexTypeHandler<T>;
+        if (complexHandler == null) return default;
+
+        T? resultObj = complexHandler.CreateNewAsType();
+
+        // Setup state machine
+        Span<char> scratchMemory = stackalloc char[1024];
+
+        JSONReadState state = JSONReadState.DetermineNextRead;
+        JSONReadState lastState = JSONReadState.None;
+        Stack<JSONStackEntry> stack = new Stack<JSONStackEntry>();
+
+        JSONStackEntry newEntry = _stackEntryPool.Get();
+        newEntry.CurrentObject = resultObj;
+        newEntry.TypeHandler = complexHandler;
+        stack.Push(newEntry);
+
+        // Start state machine!
+        while (state != JSONReadState.None)
+        {
+            JSONReadState stateAtStart = state;
+
+            switch (state)
+            {
+                // Determine what value to read next based on the next non-whitespace character.
+                case JSONReadState.DetermineNextRead:
+                    {
+                        char nextNonWhitespace = reader.MoveCursorToNextOccuranceOfNotWhitespace();
+                        if (nextNonWhitespace == '{')
+                            state = JSONReadState.StartObject;
+                        else if (nextNonWhitespace == '[')
+                            state = JSONReadState.StartArray;
+                        else if (nextNonWhitespace == '\"')
+                            state = JSONReadState.ReadString;
+                        else if (nextNonWhitespace == 't' || nextNonWhitespace == 'f') // true or false
+                            state = JSONReadState.ReadBoolean;
+                        else if ((nextNonWhitespace >= '0' && nextNonWhitespace <= '9') || nextNonWhitespace == '-')
+                            state = JSONReadState.ReadNumber;
+                        else
+                            state = JSONReadState.None;
+                    }
+                    break;
+
+                // Unwind the stack
+                case JSONReadState.PostReadValue:
+                    {
+                        char nextNonWhitespace = reader.MoveCursorToNextOccuranceOfNotWhitespace();
+
+                        JSONStackEntry currentVal = stack.Peek();
+                        if (currentVal.ParentIsList)
+                        {
+                            // This is an array - go to the next item (which should be of the same type)
+                            if (nextNonWhitespace == ',')
+                            {
+                                reader.ReadNextChar();
+                                nextNonWhitespace = reader.MoveCursorToNextOccuranceOfNotWhitespace();
+
+                                // Shortcut to reading more of the same item
+                                if (lastState == JSONReadState.ReadNumber || lastState == JSONReadState.ReadNumber)
+                                    state = lastState;
+                                else
+                                    state = JSONReadState.DetermineNextRead;
+                            }
+                            else if (nextNonWhitespace == ']')
+                            {
+                                // close array
+
+                                // pop the generic item object
+                                JSONStackEntry popped = stack.Pop(); 
+                                _stackEntryPool.Return(popped);
+
+                                // The object value being closed
+                                JSONStackEntry current = stack.Peek();
+                                IGenericEnumerableTypeHandler? handler = current.TypeHandler as IGenericEnumerableTypeHandler;
+                                if (current.ParentObject != null && handler != null && current.CurrentObject is IList tempList)
+                                {
+                                    object? fromList = handler.CreateNewFromList(tempList);
+                                    handler.ReturnTempListToPool(tempList);
+
+                                    // Set the new object as the parent's member
+                                    if (current.ParentIsList)
+                                    {
+                                        if (current.ParentObject is IList parentList)
+                                            parentList.Add(fromList);
+                                    }
+                                    else
+                                    {
+                                        current.ObjectMember?.SetValueInComplexObject(current.ParentObject, fromList);
+                                    }
+                                }
+                                state = JSONReadState.PostReadValue;
+                                reader.ReadNextChar(); // Skip closing bracket
+                            }
+                            else
+                            {
+                                // This is a bug
+                                Assert(false);
+                                state = JSONReadState.None;
+                            }
+                        }
+                        else // Object value
+                        {
+                            // Read the next key value
+                            if (nextNonWhitespace == ',')
+                            {
+                                JSONStackEntry popped = stack.Pop();
+                                _stackEntryPool.Return(popped);
+
+                                state = JSONReadState.ReadObjectKeyValue;
+                                reader.ReadNextChar(); // comma
+                            }
+                            else if (nextNonWhitespace == '}')
+                            {
+                                // close object
+
+                                // pop the member value read last
+                                // (todo: test for empty objects '{}')
+                                JSONStackEntry popped = stack.Pop();
+                                _stackEntryPool.Return(popped); 
+
+                                // The object value being closed
+                                JSONStackEntry current = stack.Peek();
+                                object? closingObj = current.CurrentObject;
+                                if (current.ParentObject != null && closingObj != null)
+                                {
+                                    // Set the new object as the parent's member
+                                    if (current.ParentIsList)
+                                    {
+                                        if (current.ParentObject is IList parentList)
+                                            parentList.Add(closingObj);
+                                    }
+                                    else
+                                    {
+                                        current.ObjectMember?.SetValueInComplexObject(current.ParentObject, closingObj);
+                                    }
+                                }
+
+                                state = JSONReadState.PostReadValue;
+                                reader.ReadNextChar(); // bracket
+                            }
+                            else
+                            {
+                                // Final object
+                                state = JSONReadState.None;
+                            }
+                        }
+                    }
+                    break;
+
+                case JSONReadState.StartObject:
+                    {
+                        // This object was pushed into the stack by the key-value read or array read.
+                        JSONStackEntry current = stack.Peek();
+                        if (current.ParentObject != null && current.TypeHandler != null)
+                        {
+                            // Initialize object for the member object
+                            IGenericReflectorComplexTypeHandler? handler = current.TypeHandler as IGenericReflectorComplexTypeHandler;
+                            object? newObj = handler?.CreateNew();
+                            current.CurrentObject = newObj;
+                        }
+
+                        // Start reading key value pairs.
+                        state = JSONReadState.ReadObjectKeyValue;
+                    }
+                    break;
+
+                case JSONReadState.ReadObjectKeyValue:
+                    {
+                        // Find next key open
+                        if (!reader.MoveCursorToNextOccuranceOfChar('\"'))
+                        {
+                            state = JSONReadState.None;
+                            break;
+                        }
+
+                        // Read key
+                        reader.ReadNextChar();
+                        int charsWritten = reader.ReadToNextOccuranceofChar('\"', scratchMemory);
+                        if (charsWritten == 0)
+                        {
+                            state = JSONReadState.None;
+                            break;
+                        }
+
+                        // Get current object data.
+                        JSONStackEntry currentData = stack.Peek();
+                        object? currentObject = currentData.CurrentObject;
+                        IGenericReflectorComplexTypeHandler? currentTypeHandler = currentData.TypeHandler as IGenericReflectorComplexTypeHandler;
+
+                        // Try to get the key handler.
+                        ComplexTypeHandlerMember? memberHandler = null;
+                        if (currentTypeHandler != null)
+                        {
+                            Span<char> nextKey = scratchMemory.Slice(0, charsWritten);
+
+                            // For JSON we enforce case insensitive keys since in C# they are
+                            // usually capitalized but in JSON they aren't.
+                            for (int i = 0; i < nextKey.Length; i++)
+                            {
+                                nextKey[i] = char.ToLowerInvariant(nextKey[i]);
+                            }
+
+                            int tagNameHash = nextKey.GetStableHashCode();
+                            memberHandler = currentTypeHandler.GetMemberByNameCaseInsensitive(tagNameHash);
+                        }
+
+                        // Find the value separator.
+                        if (!reader.MoveCursorToNextOccuranceOfChar(':'))
+                        {
+                            state = JSONReadState.None;
+                            break;
+                        }
+                        reader.ReadNextChar();
+
+                        // Read value
+                        JSONStackEntry valueReadEntry = _stackEntryPool.Get();
+                        valueReadEntry.ParentObject = currentObject;
+                        valueReadEntry.ObjectMember = memberHandler;
+                        valueReadEntry.TypeHandler = memberHandler?.GetTypeHandler();
+                        stack.Push(valueReadEntry);
+                        state = JSONReadState.DetermineNextRead;
+                    }
+                    break;
+
+                case JSONReadState.StartArray:
+                    {
+                        // This object was pushed into the stack by the key-value read or array read.
+                        JSONStackEntry current = stack.Peek();
+                        IGenericEnumerableTypeHandler? handler = current.TypeHandler as IGenericEnumerableTypeHandler;
+                        if (current.ParentObject != null && handler != null)
+                        {
+                            // Initialize temporary list to write in from a typed pool to prevent boxing
+                            IList tempList = handler.CreateTempListFromPool();
+                            current.CurrentObject = tempList;
+                        }
+
+                        // Skip opening brace
+                        reader.ReadNextChar();
+
+                        // Push a generic array member as the stack value and start reading items
+                        IGenericReflectorTypeHandler? elementHandler = null;
+                        if (handler?.ItemType != null)
+                            elementHandler = ReflectorEngine.GetTypeHandler(handler.ItemType);
+
+                        // Read value
+                        JSONStackEntry genericItemEntry = _stackEntryPool.Get();
+                        genericItemEntry.ParentObject = current.CurrentObject;
+                        genericItemEntry.ParentIsList = true;
+                        genericItemEntry.TypeHandler = elementHandler;
+                        stack.Push(genericItemEntry);
+                        state = JSONReadState.DetermineNextRead;
+                    }
+                    break;
+
+                case JSONReadState.ReadString:
+                    {
+                        char openingChar = reader.ReadNextChar();
+                        if (openingChar != '\"')
+                        {
+                            state = JSONReadState.None;
+                            break;
+                        }
+
+                        int stringChars = reader.ReadToNextOccuranceofChar('\"', scratchMemory);
+                        if (stringChars == 0)
+                        {
+                            state = JSONReadState.None;
+                            break;
+                        }
+
+                        // todo: big strings will fuck us up :/
+                        Span<char> stringContent = scratchMemory.Slice(0, stringChars);
+
+                        char closing = reader.ReadNextChar();
+                        if (closing != '\"')
+                        {
+                            state = JSONReadState.None;
+                            break;
+                        }
+
+                        // Allocate string
+                        var val = new string(stringContent);
+
+                        // Set value in parent (if any).
+                        JSONStackEntry currentVal = stack.Peek();
+                        if (currentVal.ParentObject != null && currentVal.TypeHandler != null)
+                        {
+                            if (currentVal.ParentIsList && currentVal.ParentObject is IList parentList)
+                                parentList.Add(val);
+                            else
+                                currentVal.ObjectMember!.SetValueInComplexObject(currentVal.ParentObject, val);
+                        }
+
+                        state = JSONReadState.PostReadValue;
+                    }
+                    break;
+
+                case JSONReadState.ReadNumber:
+                    {
+                        JSONStackEntry current = stack.Peek();
+                        object? parentObj = current.ParentObject;
+                        IGenericReflectorTypeHandler? handler = current.TypeHandler;
+                        if (parentObj != null && handler != null)
+                        {
+                            if (current.ParentIsList)
+                                handler.ReadValueFromStringIntoList(ref reader, parentObj);
+                            else if (current.ObjectMember != null)
+                                handler.ReadValueFromStringIntoObjectMember(ref reader, parentObj, current.ObjectMember);
+                            else
+                                reader.ReadNumber(scratchMemory);
+                        }
+                        else
+                        {
+                            reader.ReadNumber(scratchMemory);
+                        }
+
+                        state = JSONReadState.PostReadValue;
+                    }
+                    break;
+
+                case JSONReadState.ReadBoolean:
+                    {
+                        reader.ReadToNextOccuranceofChar('e', scratchMemory);
+                        reader.ReadNextChar(); // next
+
+                        bool value = scratchMemory[0] == 't';
+
+                        JSONStackEntry current = stack.Peek();
+                        object? parentObj = current.ParentObject;
+                        if (parentObj != null)
+                        {
+                            if (current.ParentIsList && parentObj is IList parentList)
+                                parentList.Add(value);
+                            else if (current.ObjectMember != null)
+                                current.ObjectMember?.SetValueInComplexObject(parentObj, value);
+                        }
+
+                        state = JSONReadState.PostReadValue;
+                    }
+                    break;
+            }
+
+            lastState = stateAtStart;
+        }
+
+        while (stack.TryPop(out JSONStackEntry? storedEntry))
+        {
+            _stackEntryPool.Return(storedEntry);
+        }
+
+        return resultObj;
     }
 
     public unsafe static T? From<T>(ref ValueStringReader reader)
