5f87ede27712f743f8d73c28a48c1e861bca7a81
Vlad Abadzhiev <TheCryru@gmail.com>
2025-04-19 09:31:03 +0300

    [One] Removed unused editors

--- Emotion/WIPUpdates/One/EditorUI/Components/EditorSelectableList.cs
+++ Emotion/WIPUpdates/One/EditorUI/Components/EditorSelectableList.cs
@@ -1,69 +1,69 @@
-﻿#nullable enable
+﻿//#nullable enable
 
-using Emotion.UI;
-using Emotion.Utility;
-using Emotion.WIPUpdates.One.EditorUI.Base;
+//using Emotion.UI;
+//using Emotion.Utility;
+//using Emotion.WIPUpdates.One.EditorUI.Base;
 
-namespace Emotion.WIPUpdates.One.EditorUI.Components;
+//namespace Emotion.WIPUpdates.One.EditorUI.Components;
 
-public class EditorSelectableList<T> : ArrayEditorBase<T>
-{
-    public bool CanEdit
-    {
-        get => _canEdit;
-        set
-        {
-            if (value == _canEdit) return;
-            _canEdit = value;
+//public class EditorSelectableList<T> : ArrayEditorBase<T>
+//{
+//    public bool CanEdit
+//    {
+//        get => _canEdit;
+//        set
+//        {
+//            if (value == _canEdit) return;
+//            _canEdit = value;
 
-            // Respawn items
-            OnItemsChanged(_items);
-        }
-    }
+//            // Respawn items
+//            OnItemsChanged(_items);
+//        }
+//    }
 
-    private bool _canEdit;
+//    private bool _canEdit;
 
-    private UIScrollArea _scrollArea;
-    private UIBaseWindow _itemList;
+//    private UIScrollArea _scrollArea;
+//    private UIBaseWindow _itemList;
 
-    public EditorSelectableList()
-    {
-        _scrollArea = new EditorScrollArea();
-        AddChild(_scrollArea);
+//    public EditorSelectableList()
+//    {
+//        _scrollArea = new EditorScrollArea();
+//        AddChild(_scrollArea);
 
-        _itemList = new UIBaseWindow()
-        {
-            LayoutMode = LayoutMode.VerticalList,
-            ListSpacing = new Vector2(0, 3)
-        };
-        _scrollArea.AddChildInside(_itemList);
-    }
+//        _itemList = new UIBaseWindow()
+//        {
+//            LayoutMode = LayoutMode.VerticalList,
+//            ListSpacing = new Vector2(0, 3)
+//        };
+//        _scrollArea.AddChildInside(_itemList);
+//    }
 
-    protected override void OnItemsChanged(IEnumerable<T>? items)
-    {
-        _itemList.ClearChildren();
+//    protected override void OnItemsChanged(IEnumerable<T>? items)
+//    {
+//        _itemList.ClearChildren();
 
-        if (items == null) return;
+//        if (items == null) return;
 
-        foreach (T item in items)
-        {
-            //_itemList.AddChild(new EditorListItem<T>(item, ChangeSelectedItem, CanEdit));
-        }
-    }
+//        foreach (T item in items)
+//        {
+//            //_itemList.AddChild(new EditorListItem<T>(item, ChangeSelectedItem, CanEdit));
+//        }
+//    }
 
-    protected override void OnSelectionChanged(T? newSel)
-    {
-        foreach (EditorListItem<T> listItem in _itemList.WindowChildren())
-        {
-            listItem.Selected = Helpers.AreObjectsEqual(listItem.Item, newSel);
-        }
-    }
+//    protected override void OnSelectionChanged(T? newSel)
+//    {
+//        foreach (EditorListItem<T> listItem in _itemList.WindowChildren())
+//        {
+//            listItem.Selected = Helpers.AreObjectsEqual(listItem.Item, newSel);
+//        }
+//    }
 
-    protected override void OnSelectionEmpty()
-    {
-        foreach (EditorListItem<T> listItem in _itemList.WindowChildren())
-        {
-            listItem.Selected = false;
-        }
-    }
-}
+//    protected override void OnSelectionEmpty()
+//    {
+//        foreach (EditorListItem<T> listItem in _itemList.WindowChildren())
+//        {
+//            listItem.Selected = false;
+//        }
+//    }
+//}
