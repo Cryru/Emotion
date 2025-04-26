a15c9595ccabebafb2671a9d352920fd434a45ab
Vlad Abadzhiev <TheCryru@gmail.com>
2025-04-26 15:53:18 +0300

    [Engine] Added an event handler for hot reloading

---
+++ Emotion/Common/EngineHotReloadHandler.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+#if DEBUG
+
+using Emotion.Game.Data;
+using Emotion.Standard.Reflector;
+
+[assembly: System.Reflection.Metadata.MetadataUpdateHandler(typeof(EngineHotReloadHandler))]
+
+namespace Emotion.Common;
+
+internal static class EngineHotReloadHandler
+{
+    public static void UpdateApplication(Type[]? updatedTypes)
+    {
+        if (updatedTypes == null) return;
+
+        ReflectorEngine.OnHotReload(updatedTypes);
+        GameDataDatabase.EditorAdapter.OnHotReload(updatedTypes);
+    }
+}
+
+#endif
