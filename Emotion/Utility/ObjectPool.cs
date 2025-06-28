23b827c6b55d12fcd92e87820a1959df28e47bff
Vlad Abadzhiev <TheCryru@gmail.com>
2025-06-28 18:01:46 +0300

    [Utility] Added a version of ObjectPool that doesn't require T to have an empty constructor

--- Emotion/Utility/ObjectPool.cs
+++ Emotion/Utility/ObjectPool.cs
@@ -6,51 +6,62 @@ using System.Collections.Concurrent;
 
 #nullable enable
 
-namespace Emotion.Utility
+namespace Emotion.Utility;
+
+/// <summary>
+/// Object pool where you must manually define the create method.
+/// Thread safe.
+/// </summary>
+public class ObjectPoolManual<T>
 {
-    /// <summary>
-    /// A pool of objects. Thread safe.
-    /// </summary>
-    public class ObjectPool<T> where T : new()
+    private readonly ConcurrentBag<T> _objects = new ConcurrentBag<T>();
+    private Func<T> _createMethod;
+    private Action<T>? _resetMethod;
+
+    public ObjectPoolManual(Func<T> createMethod, Action<T>? resetMethod = null, int createInAdvance = -1)
     {
-        private readonly ConcurrentBag<T> _objects = new ConcurrentBag<T>();
-        private Func<T> _createMethod;
-        private Action<T>? _resetMethod;
+        _createMethod = createMethod;
+        _resetMethod = resetMethod;
 
-        public ObjectPool() : this(() => new T())
+        for (var i = 0; i < createInAdvance; i++)
         {
+            _objects.Add(_createMethod());
         }
+    }
 
-        public ObjectPool(Action<T>? resetMethod = null, int preCreate = -1) : this(() => new T(), resetMethod, preCreate)
-        {
-        }
+    public T Get()
+    {
+        return _objects.TryTake(out T? item) ? item : _createMethod();
+    }
 
-        public ObjectPool(Func<T> createMethod, Action<T>? resetMethod = null, int createInAdvance = -1)
-        {
-            _createMethod = createMethod;
-            _resetMethod = resetMethod;
-
-            for (var i = 0; i < createInAdvance; i++)
-            {
-                _objects.Add(_createMethod());
-            }
-        }
-
-        public T Get()
-        {
-            return _objects.TryTake(out T? item) ? item : _createMethod();
-        }
-
-        public void Return(T item)
-        {
+    public void Return(T item)
+    {
 #if DEBUG_OBJECTPOOL
-            foreach (T obj in _objects)
-            {
-                Debug.Assert(!obj.Equals(item));
-            }
-#endif
-            _resetMethod?.Invoke(item);
-            _objects.Add(item);
+        foreach (T obj in _objects)
+        {
+            Debug.Assert(!obj.Equals(item));
         }
+#endif
+        _resetMethod?.Invoke(item);
+        _objects.Add(item);
+    }
+}
+
+/// <summary>
+/// A pool of objects. Thread safe.
+/// </summary>
+public class ObjectPool<T> : ObjectPoolManual<T>
+    where T : new()
+{
+    public ObjectPool() : base(() => new T())
+    {
+    }
+
+    public ObjectPool(Action<T>? resetMethod = null, int preCreate = -1) : base(() => new T(), resetMethod, preCreate)
+    {
+    }
+
+    public ObjectPool(Func<T> createMethod, Action<T>? resetMethod = null, int createInAdvance = -1) : base(createMethod, resetMethod, createInAdvance)
+    {
     }
 }
