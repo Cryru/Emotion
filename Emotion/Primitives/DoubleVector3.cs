96758de45260828905039d99ae09675d35b6336e
Vlad Abadzhiev <TheCryru@gmail.com>
2025-08-16 11:26:20 +0300

    [Primitives] Initial implementations of DoubleVector3 and IntVector2

---
+++ Emotion/Primitives/DoubleVector3.cs
@@ -0,0 +1,42 @@
+﻿#nullable enable
+
+using Standart.Hash.xxHash;
+
+namespace Emotion.Primitives;
+
+public struct DoubleVector3
+{
+    public double X;
+    public double Y;
+    public double Z;
+
+    public DoubleVector3(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public unsafe override int GetHashCode()
+    {
+        DoubleVector3 localThis = this; // Copy to stack
+        DoubleVector3* thisPtr = &localThis;
+        ReadOnlySpan<byte> asSpan = new ReadOnlySpan<byte>(thisPtr, sizeof(double) * 3);
+        return (int) xxHash32.ComputeHash(asSpan, asSpan.Length);
+    }
+
+    public Vector2 ToVec2()
+    {
+        return new Vector2((float) X, (float) Y);
+    }
+
+    public Vector3 ToVec3()
+    {
+        return new Vector3((float) X, (float) Y, (float) Z);
+    }
+
+    public static DoubleVector3 operator *(DoubleVector3 a, DoubleVector3 b)
+    {
+        return new DoubleVector3(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+    }
+}
---
+++ Emotion/Primitives/IntVector2.cs
@@ -0,0 +1,47 @@
+﻿#nullable enable
+
+namespace Emotion.Primitives;
+
+public struct IntVector2
+{
+    public int X;
+    public int Y;
+
+    public IntVector2(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override int GetHashCode()
+    {
+        return Maths.GetCantorPair(X, Y);
+    }
+
+    public Vector2 ToVec2()
+    {
+        return new Vector2(X, Y);
+    }
+
+    public Vector3 ToVec3(float z = 0)
+    {
+        return new Vector3(X, Y, z);
+    }
+
+    public static IntVector2 FromVec2Floor(Vector2 vec)
+    {
+        vec = vec.Floor();
+        return new IntVector2((int) vec.X, (int) vec.Y);
+    }
+
+    public static IntVector2 operator /(IntVector2 iv2, Vector2 v2)
+    {
+        Vector2 iv2F = iv2.ToVec2();
+        return FromVec2Floor(iv2F);
+    }
+
+    public static IntVector2 operator *(IntVector2 a, IntVector2 b)
+    {
+        return new IntVector2(a.X * b.X, a.Y * b.Y);
+    }
+}
