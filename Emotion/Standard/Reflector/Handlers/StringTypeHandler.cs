14cb0f186c7ea00310445b0353f58558e3716779
Vlad Abadzhiev <TheCryru@gmail.com>
2025-11-15 22:37:11 +0200

    [Reflector] String XML serialization

--- Emotion/Standard/Reflector/Handlers/StringTypeHandler.cs
+++ Emotion/Standard/Reflector/Handlers/StringTypeHandler.cs
@@ -1,9 +1,13 @@
 ﻿#nullable enable
 
-using System.Text.Json;
 using Emotion.Editor.EditorUI.ObjectPropertiesEditorHelpers;
 using Emotion.Standard.DataStructures.OptimizedStringReadWrite;
 using Emotion.Standard.Reflector.Handlers.Base;
+using Emotion.Standard.Serialization.XML;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace Emotion.Standard.Reflector.Handlers;
 
@@ -35,6 +39,40 @@ public sealed class StringTypeHandler : ReflectorTypeHandlerBase<string>
     public override void WriteAsCode(string value, ref ValueStringWriter writer)
     {
         writer.WriteString($"\"{value}\"");
+    }
+
+    public override void WriteAsXML(string value, ref ValueStringWriter writer, bool addTypeTags, XMLConfig config, int indent = 0)
+    {
+        if (addTypeTags)
+        {
+            if (!writer.WriteChar('<')) return;
+            if (!writer.WriteString(Type.Name)) return;
+            if (!writer.WriteChar('>')) return;
+        }
+
+        writer.WriteString(XMLSanitizeString(value));
+
+        if (addTypeTags)
+        {
+            if (!writer.WriteChar('<')) return;
+            if (!writer.WriteString(Type.Name)) return;
+            if (!writer.WriteChar('>')) return;
+        }
+    }
+
+    private static readonly Regex StringSanitizeRegex = new Regex("<", RegexOptions.Compiled);
+    private static readonly Regex StringRestoreRegex = new Regex("&lt;", RegexOptions.Compiled);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static string XMLSanitizeString(string str)
+    {
+        return StringSanitizeRegex.Replace(str, "&lt;");
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static string XMLRestoreString(string str)
+    {
+        return StringRestoreRegex.Replace(str, "<");
     }
 
     #endregion
