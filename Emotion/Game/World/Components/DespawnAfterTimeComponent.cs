c14b97e114ec5313be14ca4b15656da9ca890f23
Vlad Abadzhiev <TheCryru@gmail.com>
2026-01-10 21:02:50 +0200

    [World] Added component for despawning a game object after a set time

---
+++ Emotion/Game/World/Components/DespawnAfterTimeComponent.cs
@@ -0,0 +1,39 @@
+﻿#nullable enable
+
+using Emotion.Core.Utility.Coroutines;
+using Emotion.Core.Utility.Time;
+
+namespace Emotion.Game.World.Components;
+
+public class DespawnAfterTimeComponent : IGameObjectComponent, IUpdateableComponent
+{
+    private GameObject? _objectAttachedTo;
+    private After _timer;
+
+    public DespawnAfterTimeComponent(int time)
+    {
+        _timer = new After(time);
+    }
+
+    public void Done(GameObject obj)
+    {
+
+    }
+
+    public Coroutine? Init(GameObject obj)
+    {
+        _objectAttachedTo = obj;
+        return null;
+    }
+
+    public void Update(float dt)
+    {
+        AssertNotNull(_objectAttachedTo);
+
+        _timer.Update(dt);
+        if (_timer.Finished)
+        {
+            _objectAttachedTo.RemoveFromMap();
+        }
+    }
+}
